8916e08478da18cf00cd48e54f4494264b3c4cd5
Ariel Behar <ariel.behar@hotmail.com>
2023-08-15 11:06:28 +0300

    Creating a layout container and base global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Nabvar from "@/components/Header/Navbar/Nabvar";
+import Navbar from "@/components/Header/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,9 +20,11 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<Nabvar />
-				{children}
-				<Footer />
+				<div className="container px-[50px] mx-auto min-h-screen flex flex-col justify-between">
+					<Navbar />
+					{children}
+					<Footer />
+				</div>
 			</body>
 		</html>
 	);
