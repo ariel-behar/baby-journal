02739860c91206e025c308bd63b33d4150d06776
Ariel Behar <ariel.behar@hotmail.com>
2023-08-15 13:55:05 +0300

    Adding additional theme colors to the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				dark: '#0d0c22',
+				darkSoft: '#2d2b42',
+				light: 'white',
+				lightSoft: '#e5e5e5',
+				customBlue: '#3673fd'
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
