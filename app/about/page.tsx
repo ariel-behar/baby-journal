bc2326425a18923bd2dd1e35cda19b3814da37b3
Ariel Behar <ariel.behar@hotmail.com>
2023-08-15 15:24:38 +0300

    Adding an image from pexels.com with the Image component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image"
 
 function AboutPage() {
-  return (
-    <div>About Page</div>
-  )
+	return (
+		<div>
+			<div className="relative w-full h-[500px]" >
+				<Image src="https://images.pexels.com/photos/22882440/pexels-photo-22882440/free-photo-of-copa-cozinha-04.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="About" fill />
+			</div>
+		</div>
+	)
 }
 
 export default AboutPage
