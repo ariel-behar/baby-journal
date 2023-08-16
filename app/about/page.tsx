43c41ba5bfbfa1d02add0c4f416a7bc0c14acb54
Ariel Behar <ariel.behar@hotmail.com>
2023-08-16 22:54:33 +0300

    Optimizing the about page for mobile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,15 +2,15 @@ import Image from "next/image"
 
 function AboutPage() {
 	return (
-		<div className="flex gap-[100px]">
-			<div className="flex-1 flex flex-col gap-[50px]">
+		<div className="flex flex-col md:flex-row gap-[100px]">
+			<div className="flex-1 flex flex-col gap-[50px] text-center md:text-left">
 				<h3 className="text-customBlue">About Agency</h3>
 
 				<h2 className="text-[54px] font-bold leading-tight">We create digital ideas that are bigger, bolder, braver and better</h2>
 
 				<p className="text-xl "> We create digital ideas that are bigger, bolder, braver, and better. We believe in good ideas, flexibility, and precision. Our team is the best consulting and finance solutions provider. We offer a wide range of web and software development services.</p>
 
-				<div className="flex items-center justify-between">
+				<div className="gap-[50px] md:gap-0 flex flex-col md:flex-row items-center justify-between">
 					<div className="flex flex-col gap-2">
 						<h3 className="text-customBlue text-2xl font-bold">10 K+</h3>
 						<p>Year of experience</p>
