8066da16174409500247a678e6363bbcad44e527
Ariel Behar <ariel.behar@hotmail.com>
2023-08-16 19:35:56 +0300

    Structuring and styling the About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,37 @@ import Image from "next/image"
 
 function AboutPage() {
 	return (
-		<div>
-			<div className="relative w-full h-[500px]" >
-				<Image src="https://images.pexels.com/photos/22882440/pexels-photo-22882440/free-photo-of-copa-cozinha-04.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="About" fill />
+		<div className="flex gap-[100px]">
+			<div className="flex-1 flex flex-col gap-[50px]">
+				<h3 className="text-customBlue">About Agency</h3>
+
+				<h2 className="text-[54px] font-bold leading-tight">We create digital ideas that are bigger, bolder, braver and better</h2>
+
+				<p className="text-xl "> We create digital ideas that are bigger, bolder, braver, and better. We believe in good ideas, flexibility, and precision. Our team is the best consulting and finance solutions provider. We offer a wide range of web and software development services.</p>
+
+				<div className="flex items-center justify-between">
+					<div className="flex flex-col gap-2">
+						<h3 className="text-customBlue text-2xl font-bold">10 K+</h3>
+						<p>Year of experience</p>
+					</div>
+					<div>
+						<h3 className="text-customBlue text-2xl font-bold">10 K+</h3>
+						<p>Year of experience</p>
+					</div>
+					<div>
+						<h3 className="text-customBlue text-2xl font-bold">10 K+</h3>
+						<p>Year of experience</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="flex-1 relative">
+				<Image
+					src="/img/about.png"
+					alt="About Image"
+					fill
+					className="object-contain"
+				/>
 			</div>
 		</div>
 	)
