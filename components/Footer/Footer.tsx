64aa7118359846d37bdd8fff5940b10090e38abc
Ariel Behar <ariel.behar@hotmail.com>
2023-08-16 21:33:19 +0300

    Fixing the flex layout of the Footer on small and large screens

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
 
 function Footer() {
 	return (
-		<div className="flex justify-between h-[100px] items-center text-gray-500">
-			<div className="font-bold">
+		<div className="flex flex-col justify-center md:flex-row md:justify-between h-[100px] items-center text-gray-500">
+			<div className="font-bold mb-2 md:mb-0">
 				Lamadev
 			</div>
-			<div className="text-sm">
+			<div className="text-sm text-center">
 				Lama Creative Thoughts Agency &copy; All Rights Reserved
 			</div>
 		</div>
