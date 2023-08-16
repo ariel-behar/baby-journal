019d3dc95c9a6cd5de710548b73f5b46a5f68e9f
Ariel Behar <ariel.behar@hotmail.com>
2023-08-16 20:17:32 +0300

    Structuring and styling the Blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,23 @@
+import PostCard from "@/components/PostCard"
 
 function BlogPage() {
-  return (
-    <div>Blog Page</div>
-  )
+	return (
+		<div className="flex flex-wrap">
+			<div className="w-full sm:w-1/2 md:w-1/3">
+				<PostCard />
+			</div>
+			<div className="w-full sm:w-1/2 md:w-1/3">
+				<PostCard />
+			</div>
+			<div className="w-full sm:w-1/2 md:w-1/3">
+				<PostCard />
+			</div>
+			<div className="w-full sm:w-1/2 md:w-1/3">
+				<PostCard />
+			</div>
+
+		</div>
+	)
 }
 
 export default BlogPage
