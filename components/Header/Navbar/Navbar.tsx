2d045b6c83207a4260419e7790b2c35e6935073d
Ariel Behar <ariel.behar@hotmail.com>
2023-08-16 21:32:47 +0300

    Adjusting the padding of the Navbar on different screens

--- components/Header/Navbar/Navbar.tsx
+++ components/Header/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavLinks from "./NavLinks"
 function Navbar() {
 
 	return (
-		<header className="h-[100px] flex flex-row justify-between items-center">
+		<header className="h-[100px] flex flex-row justify-between items-center px-3 lg:px-0">
 			<Link href="/" className="text-3xl font-bold">Logo</Link>
 			<NavLinks />
 		</header>
