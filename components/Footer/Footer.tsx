d0ce93015577fd52a52ce1bd1266c47c5e3a4f90
Ariel Behar <ariel.behar@hotmail.com>
2023-08-16 18:58:52 +0300

    Structuring and styling the Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,15 @@
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<div className="flex justify-between h-[100px] items-center text-gray-500">
+			<div className="font-bold">
+				Lamadev
+			</div>
+			<div className="text-sm">
+				Lama Creative Thoughts Agency &copy; All Rights Reserved
+			</div>
+		</div>
+	)
 }
 
 export default Footer
