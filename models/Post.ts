ae2e8a472ceb3bc5b06c9b6875c3bce3061fc019
Ariel Behar <ariel.behar@hotmail.com>
2023-08-17 12:57:59 +0300

    Implementing the common types into the Post model

--- models/Post.ts
+++ models/Post.ts
@@ -1,6 +1,8 @@
-export interface Post {
-	userId: number;
-	id: number;
+import { Identifiable, idType } from "@/types/common-types";
+
+export interface Post extends Identifiable {
+	userId: idType;
+	id: idType;
 	title: string;
 	body: string;
 }
