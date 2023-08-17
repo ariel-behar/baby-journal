63560c74564ceaf76574c7f7b21e4ad1c653dbe2
Ariel Behar <ariel.behar@hotmail.com>
2023-08-17 13:03:53 +0300

    Implementing the individual blog post page and fetching its relevant data

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,28 @@
+import PostUser from "@/components/PostUser";
+import { Post } from "@/models/Post";
 import Image from "next/image"
 
-function SinglePostPage() {
+const getData = async (slug: string) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+	if(!res.ok) {
+		throw new Error('Something went wrong');
+	}
+
+	return res.json();
+}
+
+interface Props {
+	params: {
+		slug: string
+	}
+}
+
+async function SinglePostPage({params}: Props) {
+	const { slug } = params;
+
+	const post: Post = await getData(slug);
+
 	return (
 		<div className="flex gap-[100px]">
 			<div className="hidden md:block flex-1 relative h-[calc(100vh-200px)]">
@@ -8,15 +30,13 @@ function SinglePostPage() {
 			</div>
 
 			<div className="flex-[2] flex flex-col gap-[50px]">
-				<h1 className="text-[64px]">Title</h1>
+				<h1 className="text-[64px]">{post.title}</h1>
 
 				<div className="relative flex gap-[20px]">
 					<Image src="https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg" alt='User image' width={50} height={50} className="object-cover rounded-[50%] h-[50px] w-[50px]" />
 
-					<div className="flex flex-col gap-[10px]">
-						<span className="text-gray-500 font-bold">Author</span>
-						<span className="font-medium">Terry Jefferson</span>
-					</div>
+					<PostUser userId={post.userId} />
+
 					<div className="flex flex-col gap-[10px]">
 						<span className="text-gray-500 font-bold">Published </span>
 						<span className="font-medium">01.01.2024s</span>
@@ -25,7 +45,7 @@ function SinglePostPage() {
 
 				<div>
 					<p className="text-xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vel aliquid pariatur libero, soluta veritatis, reprehenderit in possimus nostrum eos odit dolorem doloribus earum? Ipsa ratione sit nostrum repudiandae dolorem.
+						{post.body}
 					</p>
 				</div>
 			</div>
