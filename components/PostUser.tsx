40f4ec882eebe05a899264e7aeef017d059512fd
Ariel Behar <ariel.behar@hotmail.com>
2023-08-17 13:04:36 +0300

    Creating a PostUser component and fetching its respective data

---
+++ components/PostUser.tsx
@@ -0,0 +1,29 @@
+import { Post } from "@/models/Post"
+import { User } from "@/models/User";
+
+const getData = async (userId: User['id']) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+	if(!res.ok) {
+		throw new Error('Something went wrong');
+	}
+
+	return res.json();
+}
+
+interface Props extends Omit<Post, 'title' | 'body' | 'id'> {}
+
+async function PostUser({
+    userId
+}:Props) {
+    const user: User = await getData(userId);
+
+    return (
+        <div className="flex flex-col gap-[10px]">
+            <span className="text-gray-500 font-bold">Author</span>
+            <span className="font-medium">{user.name}</span>
+        </div>
+    )
+}
+
+export default PostUser
