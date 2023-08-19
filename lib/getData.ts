1f49e065086c6a793f621317ca4839cbdd36d8e0
Ariel Behar <ariel.behar@hotmail.com>
2023-08-19 18:04:53 +0300

    Modifying the getData file, so it uses the newly created mongoose models to fetch from the database

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,24 +1,56 @@
-import { IPost } from "@/models/Post";
+import Post, { IPost } from "@/models/Post";
 import { IdType } from "@/types/common-types";
 import dbConnect from "./dbConnect";
+import User, { IUser } from "@/models/User";
+
+export const getPost = async (postId: IdType) => {
+    try {
+        dbConnect();
+
+        const post:IPost | never[] = await Post.find({ _id: postId });
+
+        return post;
+    } catch(error) {
+        console.error(error);
+        throw new Error('Failed to fetch post!');
+    }
+}
 
 export const getPosts = async () => {
     try {
         dbConnect();
 
-        const posts = await Post.find();
+        const posts:IPost[] | [] = await Post.find();
 
+        return posts;
     } catch(error) {
         console.error(error);
-        throw new Error('Failed to fetch posts');
+        throw new Error('Failed to fetch posts!');
     }
 }
 
-export const getPost = async (postId: IdType) => {
-    return posts.find(post => post.id === Number(postId));
-}
-
 export const getUser = async (userId: IdType) => {
-    return users.find(user => user.id === Number(userId));
+    try {
+        dbConnect();
+
+        const user:IUser | never[] = await User.find({ _id: userId });
+
+        return user;
+    } catch(error) {
+        console.error(error);
+        throw new Error('Failed to fetch user data!');
+    }
 }
 
+export const getUsers = async () => {
+    try {
+        dbConnect();
+
+        const users:IUser[] | [] = await User.find();
+
+        return users;
+    } catch(error) {
+        console.error(error);
+        throw new Error('Failed to fetch users!');
+    }
+}
