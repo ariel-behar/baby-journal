3b3af36c9b253694e51e9626d1ecf8d847d5bfe4
Ariel Behar <ariel.behar@hotmail.com>
2023-08-19 17:51:17 +0300

    Creating the Post model mongoose schema

--- models/Post.ts
+++ models/Post.ts
@@ -1,8 +1,48 @@
-import { Identifiable, idType } from "@/types/common-types";
+import { Identifiable, IdType } from "@/types/common-types";
+import mongoose from "mongoose";
 
 export interface Post extends Identifiable {
-	userId: idType;
-	id: idType;
+	_id: IdType;
 	title: string;
-	body: string;
+	description: string;
+	img: string;
+	userId: IdType;
+	slug: string;
 }
+
+interface IPostMongooseSchema extends Omit<Post, '_id' | 'userId'> {
+	userId: mongoose.Schema.Types.ObjectId;
+}
+
+const postSchema = new mongoose.Schema<IPostMongooseSchema>({
+	title: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 50
+	},
+	description: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 200
+	},
+	img: {
+		type: String,
+		required: true
+	},
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	slug: {
+		type: String,
+		unique: true,
+		required: true
+	}
+}, { timestamps: true });
+
+const Post = mongoose.models.Post || mongoose.model<IPostMongooseSchema>('Post', postSchema);
+
+export default Post
