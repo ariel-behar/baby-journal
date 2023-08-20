a226fb2abf604f0580e4587829adcac2f5d55899
Ariel Behar <ariel.behar@hotmail.com>
2023-08-20 20:04:03 +0300

    Displaying the user's avatar image

--- app/blog/[postId]/page.tsx
+++ app/blog/[postId]/page.tsx
@@ -32,8 +32,6 @@ async function SinglePostPage({ params }: Props) {
 				<h1 className="text-[64px]">{post?.title}</h1>
 
 				<div className="relative flex gap-[20px]">
-					<Image src="https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg" alt='User image' width={50} height={50} className="object-cover rounded-[50%] h-[50px] w-[50px]" />
-
 					{
 						post && (
 							<Suspense fallback={<div>Loading...</div>}>
@@ -41,7 +39,6 @@ async function SinglePostPage({ params }: Props) {
 							</Suspense>
 						)
 					}
-
 					<div className="flex flex-col gap-[10px]">
 						<span className="text-gray-500 font-bold">Published</span>
 						<span className="font-medium">01.01.2024</span>
--- components/PostUser.tsx
+++ components/PostUser.tsx
@@ -1,20 +1,26 @@
+import Image from "next/image"
+
 import { IPost } from "@/models/Post"
 import { IUser } from "@/models/User";
 
 import { getUser } from "@/lib/getData";
 
-interface Props extends Partial<IPost> {}
+interface Props extends Partial<IPost> { }
 
 async function PostUser({
     userId
-}:Props) {
+}: Props) {
     const user: IUser | null = await getUser(userId as string);
 
     return (
-        <div className="flex flex-col gap-[10px]">
-            <span className="text-gray-500 font-bold">Author</span>
-            <span className="font-medium">{user?.username}</span>
-        </div>
+        <>
+            <Image src={user?.avatarImg ? user?.avatarImg : "https://images.pexels.com/photos/18505361/pexels-photo-18505361/free-photo-of-woman-in-avatar-cosplay.jpeg"} alt='User image' width={50} height={50} className="object-cover rounded-[50%] h-[50px] w-[50px]" />
+
+            <div className="flex flex-col gap-[10px]">
+                <span className="text-gray-500 font-bold">Author</span>
+                <span className="font-medium">{user?.username}</span>
+            </div>
+        </>
     )
 }
 
