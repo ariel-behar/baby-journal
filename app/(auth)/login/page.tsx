d3fb00503c600f05e8b395082023a525156ed25f
Ariel Behar <ariel.behar@hotmail.com>
2023-08-22 09:44:22 +0300

    Implementing a test login button in the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,22 @@
-function LoginPage() {
-  return (
-    <div>Login Page</div>
-  )
+import { auth, signIn } from "@/lib/auth";
+
+async function LoginPage() {
+	const session = await auth();
+	console.log('session:', session)
+
+	const handleGithubLogin = async () => {
+		"use server"
+
+		await signIn("github");
+	}
+
+	return (
+		<div>
+			<form action={handleGithubLogin}>
+				<button>Login with Github</button>
+			</form>
+		</div>
+	)
 }
 
 export default LoginPage
