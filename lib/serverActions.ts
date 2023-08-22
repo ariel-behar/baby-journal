d3d34e08c7e0eef493f7093387c873fb2d6930f2
Ariel Behar <ariel.behar@hotmail.com>
2023-08-22 11:43:08 +0300

    Creating the addPost and deletePost server actions

---
+++ lib/serverActions.ts
@@ -0,0 +1,40 @@
+"use server"
+import Post from "@/models/Post";
+import dbConnect from "./dbConnect";
+import { revalidatePath } from "next/cache";
+
+export const addPost = async (formData: FormData) => {
+    // "use server"
+
+    const { title, description, userId, img } = Object.fromEntries(formData);
+
+    try {
+        dbConnect();
+        const newPost = new Post({ title, description, img, userId });
+
+        await newPost.save();
+        console.log('Post added successfully');
+        revalidatePath('/blog');
+    } catch (error) {
+        console.log(error);
+        return { error: "Something went wrong!"}
+    }
+}
+
+export const deletePost = async (formData: FormData) => {
+    // "use server"
+
+    const { postId } = Object.fromEntries(formData);
+
+    try {
+        dbConnect();
+
+        await Post.findByIdAndDelete(postId);
+
+        console.log('Post delete successfully');
+        revalidatePath('/blog');
+    } catch (error) {
+        console.log(error);
+        return { error: "Something went wrong!"}
+    }
+}
