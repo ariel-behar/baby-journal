09684aa504855585b414ef544305af57151bb709
Ariel Behar <ariel.behar@hotmail.com>
2023-08-25 21:56:00 +0300

    Extracting the login form into a separate component

---
+++ components/Forms/LoginForm.tsx
@@ -0,0 +1,13 @@
+import { login } from '@/lib/serverActions'
+
+function LoginForm() {
+    return (
+        <form action={login} className="flex flex-col text-center gap-7">
+            <input className="form-input" type="text" placeholder="Username" name="username" />
+            <input className="form-input" type="password" placeholder="password" name="password" />
+            <button className="blue-button">Login with Credentials</button>
+        </form>
+    )
+}
+
+export default LoginForm
