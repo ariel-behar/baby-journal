464cadba4d5061e39e94fb1a823a3d5c2bead6c0
Ariel Behar <ariel.behar@hotmail.com>
2023-08-25 21:54:33 +0300

    Extracting the register form into a separate component

---
+++ components/Forms/RegisterForm.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { register } from "@/lib/serverActions"
+
+function RegisterForm() {
+
+    return (
+        <form action={register} className="flex flex-col text-center gap-7">
+            <input className="form-input" type="text" placeholder="Username" name="username" />
+            <input className="form-input" type="text" placeholder="First Name" name="firstName" />
+            <input className="form-input" type="text" placeholder="Last Name" name="lastName" />
+            <input className="form-input" type="email" placeholder="Email" name="email" />
+            <input className="form-input" type="password" placeholder="Password" name="password" />
+            <input className="form-input" type="password" placeholder="Repeat Password" name="repeatPassword" />
+
+            <button className="blue-button">Register</button>
+        </form>
+    )
+}
+
+export default RegisterForm
