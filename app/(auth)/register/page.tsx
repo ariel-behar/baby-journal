010eaecf08fcad5edb16271a3d30bab393b79bcd
Ariel Behar <ariel.behar@hotmail.com>
2023-08-25 09:55:05 +0300

    Styling the register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,22 @@
+import { register } from "@/lib/serverActions"
+
 function RegisterPage() {
-  return (
-    <div>Register Page</div>
-  )
+	return (
+		<div className="flex items-center justify-center">
+			<div className="w-[500px] bg-darkSoft p-12 flex flex-col text-center gap-7 rounded-md ">
+				<form action={register} className="flex flex-col text-center gap-7">
+					<input className="form-input" type="text" placeholder="Username" name="username" />
+					<input className="form-input" type="text" placeholder="First Name" name="firstName" />
+					<input className="form-input" type="text" placeholder="Last Name" name="lastName" />
+					<input className="form-input" type="email" placeholder="Email" name="email" />
+					<input className="form-input" type="password" placeholder="Password" name="password" />
+					<input className="form-input" type="password" placeholder="Repeat Password" name="repeatPassword" />
+
+					<button className="blue-button">Register</button>
+				</form>
+			</div>
+		</div>
+	)
 }
 
 export default RegisterPage
