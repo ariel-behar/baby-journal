7eb5f066ac6353331022929e4975c8b0a3dc5453
Ariel Behar <ariel.behar@hotmail.com>
2023-08-25 21:55:35 +0300

    Adding the RegisterForm into the register page and adding a link button to the login page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,19 +1,18 @@
-import { register } from "@/lib/serverActions"
+import RegisterForm from "@/components/Forms/RegisterForm"
+import Link from "next/link"
 
 function RegisterPage() {
 	return (
 		<div className="flex items-center justify-center">
 			<div className="w-[500px] bg-darkSoft p-12 flex flex-col text-center gap-7 rounded-md ">
-				<form action={register} className="flex flex-col text-center gap-7">
-					<input className="form-input" type="text" placeholder="Username" name="username" />
-					<input className="form-input" type="text" placeholder="First Name" name="firstName" />
-					<input className="form-input" type="text" placeholder="Last Name" name="lastName" />
-					<input className="form-input" type="email" placeholder="Email" name="email" />
-					<input className="form-input" type="password" placeholder="Password" name="password" />
-					<input className="form-input" type="password" placeholder="Repeat Password" name="repeatPassword" />
+				<RegisterForm />
 
-					<button className="blue-button">Register</button>
-				</form>
+				<span>
+					Already an existing user?&nbsp;
+					<Link href="/login" className="font-bold">
+						Login here!
+					</Link>
+				</span>
 			</div>
 		</div>
 	)
