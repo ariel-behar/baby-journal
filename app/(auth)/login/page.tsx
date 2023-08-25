2d724ba21b8ea411f2fa9fa529feba870af868d0
Ariel Behar <ariel.behar@hotmail.com>
2023-08-25 12:15:05 +0300

    Implementing the login functionality in the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { handleGithubLogin } from "@/lib/serverActions";
+import { handleGithubLogin, login } from "@/lib/serverActions";
 
 async function LoginPage() {
 
@@ -6,6 +6,12 @@ async function LoginPage() {
 		<div>
 			<form action={handleGithubLogin}>
 				<button>Login with Github</button>
+			</form>
+
+			<form action={login}>
+				<input className="form-input" type="text" placeholder="Username" name="username" />
+				<input className="form-input" type="password" placeholder="password" name="password" />
+				<button className="blue-button">Login with Credentials</button>
 			</form>
 		</div>
 	)
