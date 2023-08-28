83582256135ae71d3559a8168246c5d857ba34ce
Ariel Behar <ariel.behar@hotmail.com>
2023-08-28 11:32:04 +0300

    Creating and implementing the admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,43 @@
-function AdminPage() {
-  return (
-    <div>Admin Page</div>
-  )
+import { Suspense } from "react"
+import { Session } from "next-auth";
+
+import { auth } from "@/lib/auth";
+
+import AdminPostForm from "@/components/Admin/AdminPostForm"
+import AdminPosts from "@/components/Admin/AdminPosts"
+import AdminUserForm from "@/components/Admin/AdminUserForm"
+import AdminUsers from "@/components/Admin/AdminUsers"
+
+async function AdminPage() {
+	const session: Session | null = await auth();
+
+	return (
+		<div className="container">
+			<div className="grid grid-cols-2 gap-5">
+				<div>
+					<Suspense fallback={<div>Loading...</div>}>
+						<AdminPosts />
+					</Suspense>
+				</div>
+
+				<div>
+					<AdminPostForm userId={session?.user?.id as string} />
+				</div>
+			</div>
+
+			<div className="grid grid-cols-2 gap-5">
+				<div>
+					<Suspense fallback={<div>Loading...</div>}>
+						<AdminUsers />
+					</Suspense>
+				</div>
+
+				<div>
+					<AdminUserForm />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AdminPage
