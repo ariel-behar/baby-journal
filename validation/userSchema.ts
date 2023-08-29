3288edf97b1dc7ad68a654e6feda4badb3cb0dea
Ariel Behar <ariel.behar@hotmail.com>
2023-08-29 21:34:23 +0300

    Creating the User yup schema validation

---
+++ validation/userSchema.ts
@@ -0,0 +1,46 @@
+import { IUser } from '@/models/User'
+import * as yup from 'yup'
+
+interface UserYupSchemaShape extends Omit<IUser, "_id" | 'createdAt' | "updatedAt"> {
+    confirmPassword: string;
+}
+
+export const userSchemaShape = {
+    username: yup
+        .string()
+        .required('Username is required')
+        .min(6, 'Username must be at least 6 characters long')
+        .max(20, 'Username must be at most 20 characters long'),
+    firstName: yup
+        .string()
+        .required('First name is required')
+        .min(2, 'Username must be at least 2 characters long')
+        .max(20, 'Username must be at most 20 characters long'),
+    lastName: yup
+        .string()
+        .required('Last name is required')
+        .min(2, 'Username must be at least 2 characters long')
+        .max(20, 'Username must be at most 20 characters long'),
+    email: yup
+        .string()
+        .email("Email is not valid")
+        .required("Email is required"),
+    password: yup
+        .string()
+        .required("Password is required")
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, 'Password must be at least 8 characters and contain at least one uppercase letter, one lowercase letter and one number')
+        .min(8, 'Password must be at least 8 characters long'),
+    confirmPassword: yup
+        .string()
+        .required("Confirm Password is required")
+        .oneOf([yup.ref("password")], "Password and Confirm Password must match"),
+    img: yup
+        .string(),
+    isAdmin: yup
+        .boolean()
+        .required('isAdmin is required')
+}
+
+export const userSchema: yup.ObjectSchema<UserYupSchemaShape> = yup.object().shape(userSchemaShape);
+
+export default userSchema
