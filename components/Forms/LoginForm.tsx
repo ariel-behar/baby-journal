68eaf9bddc6cdb52f288260e9ca6cf03c78a50a2
Ariel Behar <ariel.behar@hotmail.com>
2023-09-01 12:02:37 +0300

    Implementing FormInputFieldWithTooltip component into the login form

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -1,11 +1,46 @@
-import { login } from '@/lib/serverActions'
+"use client"
+import { login } from '@/lib/serverActions';
+import { userLoginSchema } from '@/validation/userSchema';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { BaseSyntheticEvent } from 'react';
+import { useForm } from 'react-hook-form';
+import FormInputFieldWithTooltip from './FormComponents/FormInputFieldWithTooltip';
+
+export interface LoginFormData {
+    username: string;
+    password: string;
+}
 
 function LoginForm() {
+    const { register, handleSubmit, reset, formState: { isDirty, isValid, errors } } = useForm<LoginFormData>({
+        resolver: yupResolver(userLoginSchema),
+        mode: 'onBlur',
+        defaultValues: {
+            username: '',
+            password: '',
+        },
+    });
+
+    console.log(isDirty);
+
+    const onFormSubmit = async (formData: LoginFormData, e: BaseSyntheticEvent<object, any, any> | undefined) => {
+        e?.preventDefault();
+
+        const { username, password } = formData;
+
+        if (username && password) {
+            const response = await login(formData);
+            console.log('response:', response)
+        }
+    }
+
     return (
-        <form action={login} className="flex flex-col text-center gap-7">
-            <input className="form-input" type="text" placeholder="Username" name="username" />
-            <input className="form-input" type="password" placeholder="password" name="password" />
-            <button className="btn btn-lg btn-primary">Login with Credentials</button>
+        <form onSubmit={handleSubmit(onFormSubmit)} className="flex flex-col text-center gap-7">
+            <FormInputFieldWithTooltip register={register} errors={errors} name="username" placeholder='Username' type='text' className='form-input w-full' />
+
+            <FormInputFieldWithTooltip register={register} errors={errors} name="password" placeholder='Password' type='password' className='form-input w-full' />
+
+            <button className="btn btn-lg btn-primary" disabled={!(isDirty && isValid)}>Login with Credentials</button>
         </form>
     )
 }
