951bfe4819507f4f869e83538bc82cd7bb41188d
Ariel Behar <ariel.behar@hotmail.com>
2023-09-02 15:01:46 +0300

    Adding a title to the register form

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,10 +1,13 @@
-import RegisterForm from "@/components/Forms/RegisterForm"
 import Link from "next/link"
+
+import RegisterForm from "@/components/Forms/RegisterForm"
 
 function RegisterPage() {
 	return (
 		<div className="flex items-center justify-center">
 			<div className="w-[500px] bg-dark-soft p-12 flex flex-col text-center gap-7 rounded-md ">
+				<h3 className="text-3xl uppercase">Register</h3>
+
 				<RegisterForm />
 
 				<span>
