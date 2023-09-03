6e4e631122c40ba2a7562eb5be50215fe9c9fa40
Ariel Behar <ariel.behar@hotmail.com>
2023-09-03 18:42:50 +0300

    Transforming the AdminUserForm with react-hook-form and form components

---
+++ components/Forms/AdminUserForm.tsx
@@ -0,0 +1,68 @@
+"use client"
+import { useForm } from "react-hook-form";
+import { BaseSyntheticEvent } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import userSchema from "@/validation/userSchema";
+import { addUser } from "@/lib/serverActions"
+
+import { IRegisterFormData } from "./RegisterForm";
+import FormSubmitButton from "./FormComponents/FormSubmitButton";
+import FormInputFieldWithTooltip from "./FormComponents/FormInputFieldWithTooltip";
+
+function AdminUserForm() {
+    const { register, handleSubmit, formState: { errors, isValid, isDirty } } = useForm<IRegisterFormData>({
+        resolver: yupResolver(userSchema),
+        mode: 'onBlur',
+        defaultValues: {
+            username: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            img: '',
+            isAdmin: false
+        },
+    });
+    
+    const onFormSubmit = async (formData: IRegisterFormData, e: BaseSyntheticEvent<object, any, any> | undefined) => {
+        e?.preventDefault();
+
+        const { username, firstName, lastName, email, password, confirmPassword, img, isAdmin } = formData;
+
+        if (username && firstName && lastName && email && password && confirmPassword) {
+            try {
+               const response = await addUser({username, firstName, lastName, email, password,confirmPassword, img, isAdmin});    
+               console.log('response:', response)
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+    return (
+        <div className="w-[500px] bg-dark-soft p-3 text-center rounded-md ">
+            <h3 className="text-xl uppercase mb-3">Add new User</h3>
+            <form onSubmit={handleSubmit(onFormSubmit)} className="flex flex-col gap-3">
+                <FormInputFieldWithTooltip register={register} errors={errors} name="username" placeholder='Username' type='text' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="firstName" placeholder='First Name' type='text' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="lastName" placeholder='Last Name' type='text' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="email" placeholder='Email' type='email' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="password" placeholder='Password' type='password' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="confirmPassword" placeholder='Confirm Password' type='password' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="img" placeholder='Image' type='text' />
+
+                <select {...register('isAdmin')} className="form-input" name="isAdmin" id="">
+                    <option value="false">User</option>
+                    <option value="true">Admin</option>
+                </select>
+                
+                <FormSubmitButton isDirty={isDirty} isValid={isValid}>Add User</FormSubmitButton>
+            </form>
+        </div>
+    )
+}
+
+export default AdminUserForm
