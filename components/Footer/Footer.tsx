d48751dd6f6a9d1894fb7cb5ddab0066584bed45
Ariel Behar <ariel.behar@hotmail.com>
2023-09-07 22:26:27 +0300

    Restructuring the Footer and adding its correct details

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,23 @@
+import Image from "next/image"
 
 function Footer() {
 	return (
-		<div className="flex flex-col justify-center md:flex-row md:justify-between h-[100px] items-center text-gray-500">
-			<div className="font-bold mb-2 md:mb-0">
-				Lamadev
+		<div className="flex flex-col justify-center sm:flex-row sm:justify-between h-[100px] items-center text-gray-500 text-sm">
+
+			<div className="mb-1 sm:mb-0 flex flex-row">
+				<span >
+					Powered by&nbsp;
+					<a href="https://www.arielbehar.com" className="font-bold mr-2 underline">
+						Ariel Behar
+					</a>
+				</span>
+				<Image src="/img/logo/arielbehar-logo.png" width={20} height={20} alt="Ariel Behar Logo" />
 			</div>
-			<div className="text-sm text-center">
-				Lama Creative Thoughts Agency &copy; All Rights Reserved
+
+			<div className="text-center">
+				{new Date().getFullYear()} &copy; All Rights Reserved
 			</div>
+
 		</div>
 	)
 }
