473c0bd048f3f8e0834a0de55baff411e70f1d19
Ariel Behar <ariel.behar@hotmail.com>
2023-09-07 09:15:08 +0300

    Updating the BlogPost component by importing the newly created button and published details' components

--- components/Blog/BlogPost.tsx
+++ components/Blog/BlogPost.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import { format } from "date-fns"
 import { Suspense } from "react"
 
 import { IPost } from "@/models/Post"
@@ -7,6 +6,8 @@ import { IPost } from "@/models/Post"
 import PostUser from "@/components/Blog/BlogPostUser"
 import { auth } from "@/lib/auth"
 import { Session } from "next-auth"
+import BlogPostOwnerButtons from "./BlogPostOwnerButtons"
+import BlogPostPublished from "./BlogPostPublished"
 
 
 interface Props {
@@ -20,44 +21,30 @@ async function BlogPost({
 
     return (
         <div className="flex gap-[100px] py-5">
-            {
-                post && (
-                    <div className="hidden md:block flex-1 relative h-[calc(100vh-200px)]">
-                        <Image src={post.img} alt='Post' fill className="object-cover" />
-                    </div>
-                )
-            }
+
+            {post && (
+                <div className="hidden md:block flex-1 relative h-[calc(100vh-200px)]">
+                    <Image src={post.img} alt='Post' fill className="object-cover" />
+                </div>
+            )}
 
             <div className="flex-[2] flex flex-col gap-[50px]">
                 <h1 className="text-[64px]">{post.title}</h1>
 
                 <div className="flex flex-row justify-between items-center">
                     <div className="relative flex gap-[20px]">
-                        {
-                            post && (
-                                <Suspense fallback={<div>Loading...</div>}>
-                                    <PostUser userId={post.userId} />
-                                </Suspense>
-                            )
-                        }
-                        <div className="flex flex-col gap-[10px]">
-                            <span className="text-gray-500 font-bold">
-                                Published
-                            </span>
-                            <span className="font-medium">
-                                {format(new Date(post.createdAt as string), "dd MMM yyyy")}
-                            </span>
-                        </div>
+                        {post && (
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <PostUser userId={post.userId} />
+                            </Suspense>
+                        )}
+
+                        <BlogPostPublished createdAt={post.createdAt} />
                     </div>
 
-                    {
-                        session?.user?.id == post.userId && (
-                            <div className="flex gap-3">
-                                <button className="btn btn-sm btn-primary">Edit</button>
-                                <button className="btn btn-sm btn-error">Delete</button>
-                            </div>
-                        )
-                    }
+                    {session?.user?.id == post.userId && (
+                        <BlogPostOwnerButtons post={post} />
+                    )}
                 </div>
 
                 <div>
