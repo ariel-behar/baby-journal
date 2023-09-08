c9ad6c92bded963a53808e14c4fbfbfcb1c4feed
Ariel Behar <ariel.behar@hotmail.com>
2023-09-08 18:34:48 +0300

    Adding a link around the post image/title and including an edit/delete buttons for each post

--- components/Admin/AdminPosts.tsx
+++ components/Admin/AdminPosts.tsx
@@ -1,24 +1,46 @@
 import { getPosts } from "@/lib/getData"
 import { deletePost } from "@/lib/serverActions";
 import Image from "next/image";
+import IconTrash from "../Icons/IconTrash";
+import { auth } from "@/lib/auth";
+import { Session } from "next-auth";
+import { ICustomSession } from "../Header/Navbar/NavLinks";
+import IconPencil from "../Icons/IconPencil";
+import Link from "next/link";
 
 async function AdminPosts() {
 	const posts = await getPosts();
+	const session: Session | null = await auth()
 
 	return (
 		<div className="container">
 			<h3>Posts</h3>
+
 			{posts.map((post) => (
 				<div key={post._id} className="my-5 flex items-center justify-between gap-5">
-					<div className="flex items-center gap-5">
+					<Link href={`/blog/${post._id}`} className="flex items-center gap-5 hover:underline">
 						<Image src={post.img || "/img/noavatar.png"} alt="" width={50} height={50} />
-						<span>{post.title}</span>
-					</div>
+						<span className="hover:underline">{post.title}</span>
+					</Link>
 
+					{(session?.user?.id == post.userId || (session as ICustomSession).user?.isAdmin) && (
+						<div>
+							<button className="btn btn-sm btn-primary btn-outline border-none">
+								<IconPencil sizeClassName="size-5" />
+							</button>
+
+
+							<button className="btn btn-sm btn-error btn-outline border-none">
+								<IconTrash sizeClassName="size-5" />
+							</button>
+
+						</div>
+					)}
+
+					{/* 
 					<form action={deletePost}>
 						<input type="hidden" name="postId" value={JSON.parse(JSON.stringify(post._id))} />
-						<button className="btn btn-sm btn-error ">Delete</button>
-					</form>
+					</form> */}
 				</div>
 			))}
 
