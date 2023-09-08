ea9e9076fcfd9fce47332840faecc19c7253f817
Ariel Behar <ariel.behar@hotmail.com>
2023-09-08 11:52:49 +0300

    Implementing a size prop to all icon components

--- components/Icons/IconCheck.tsx
+++ components/Icons/IconCheck.tsx
@@ -1,6 +1,12 @@
-function IconCheck() {
+interface Props {
+    size?: number
+}
+
+function IconCheck({
+    size = 6
+}:Props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-${size}`}>
             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
         </svg>
     )
--- components/Icons/IconClose.tsx
+++ components/Icons/IconClose.tsx
@@ -1,6 +1,12 @@
-function IconClose() {
+interface Props {
+    size?: number
+}
+
+function IconClose({
+    size = 6
+}:Props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-${size}`}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
 
