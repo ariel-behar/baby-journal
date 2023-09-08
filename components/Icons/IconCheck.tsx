1efb9ef6680b95a9fede49cfd788d5a42c388663
Ariel Behar <ariel.behar@hotmail.com>
2023-09-08 18:00:44 +0300

    Switiching the size prop for the icon components with a fixed format string class

--- components/Icons/IconCheck.tsx
+++ components/Icons/IconCheck.tsx
@@ -1,12 +1,11 @@
 interface Props {
-    size?: number
+    sizeClassName?: "size-1" | "size-2" | "size-3" | "size-4" | "size-5" | "size-6" | "size-7" | "size-8" | "size-9" | "size-10"
 }
-
 function IconCheck({
-    size = 6
+    sizeClassName = 'size-6'
 }:Props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-${size}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sizeClassName}>
             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
         </svg>
     )
