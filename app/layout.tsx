dd407f5651b3bef9350ec37d9cdb6bee68fd795e
Ariel Behar <ariel.behar@hotmail.com>
2023-10-04 16:35:56 +0300

    Implementing the Providers and UserConfirmationModal into the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import Navbar from "@/components/Header/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import Providers from "@/components/Providers";
+import UserConfirmationModal from "@/components/Modal/UserConfirmationModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,27 +13,32 @@ export const metadata: Metadata = {
 	title: {
 		default: 'My Blog | Home',
 		template: "My Blog | %s",
-		
+
 	},
 	description: "Home page description",
 };
 
 export default function RootLayout({
 	children,
+	modal
 }: Readonly<{
 	children: React.ReactNode;
+	modal: React.ReactNode;
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} text-light`}>
-				<div className="lg:container px-3 sm:px-5 lg:px-[50px] mx-auto min-h-screen flex flex-col justify-between">
-					<Navbar />
-					<main>
-						{children}
-					</main>
-					<Footer />
-				</div>
-			</body>
+			<Providers>
+				<body className={`${inter.className} text-light`}>
+					<div className="lg:container px-3 sm:px-5 lg:px-[50px] mx-auto min-h-screen flex flex-col justify-between">
+						<Navbar />
+						<main>
+							{children}
+						</main>
+						<Footer />
+					</div>
+					<UserConfirmationModal />
+				</body>
+			</Providers>
 		</html>
 	);
 }
