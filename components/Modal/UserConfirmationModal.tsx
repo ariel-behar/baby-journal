1ea3040201e0438b59d33780bd5892401c7b8f23
Ariel Behar <ariel.behar@hotmail.com>
2023-10-04 14:00:13 +0300

    Creating the UserConfirmationModal and implementing the modalContext into its functionality

--- components/Modal/UserConfirmationModal.tsx
+++ components/Modal/UserConfirmationModal.tsx
@@ -1,23 +1,25 @@
-import { IPost } from "@/models/Post"
+"use client"
+import { useEffect } from "react"
+import { useModalContext } from "@/context/modalContext"
+
 import IconTrash from "../Icons/IconTrash"
 
-interface Props {
-    post: IPost
-    deletePostHandler: (confirm: boolean) => void
-}
+function UserConfirmationModal() {
+    const { showModal, post, showModalHandler, deletePostHandler } = useModalContext()
 
-function UserConfirmationModal({
-    post,
-    deletePostHandler
-}: Props) {
+    useEffect(() => {
+        if(showModal) {
+            (document.getElementById('confirm-modal') as HTMLDialogElement).showModal()
+        }
+    },[showModal])
 
     return (
-        <dialog id="confirm-modal" className="modal">
+        <dialog id="confirm-modal" className="modal" onClose={() => showModalHandler(false, null)}>
             <div className="modal-box bg-secondary text-secondary-content">
 
                 <p className="py-4">
                     Are you sure you want to delete post&nbsp;
-                    <span className="font-bold">"{post.title}"</span>
+                    <span className="font-bold">"{post?.title}"</span>
                     &nbsp;?
                 </p>
 
@@ -25,8 +27,9 @@ function UserConfirmationModal({
                     <form method="dialog" className="flex justify-center w-full gap-1 sm:gap-10">
                         {/* if there is a button in form, it will close the modal */}
                         <button className="btn btn-sm btn-error btn-outline" onClick={() => deletePostHandler(true)}>
-                            Delete Post <IconTrash size={5} />
+                            Delete Post <IconTrash sizeClassName="size-5" />
                         </button>
+
                         <button className="btn btn-sm btn-primary sm:px-7" onClick={() => deletePostHandler(false)}>Cancel</button>
                     </form>
                 </div>
