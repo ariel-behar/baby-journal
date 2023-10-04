2254eeb42be6849494f88511a1a226c43c2f6138
Ariel Behar <ariel.behar@hotmail.com>
2023-10-04 18:19:00 +0300

    User confirmation modal now works to delete a user and also a post

--- components/Modal/UserConfirmationModal.tsx
+++ components/Modal/UserConfirmationModal.tsx
@@ -3,24 +3,33 @@ import { useEffect, useRef } from "react"
 import { useModalContext } from "@/context/modalContext"
 
 import IconTrash from "../Icons/IconTrash"
+import { IPost } from "@/models/Post"
+import { IUser } from "@/models/User"
 
 function UserConfirmationModal() {
-    const { showModal, post, showModalHandler, deletePostHandler } = useModalContext()
+    const { showModal, currentEntity, showModalHandler, deletePostHandler } = useModalContext()
     const modalRef = useRef<HTMLDialogElement | null>(null)
 
     useEffect(() => {
-        if(showModal) {
+        if (showModal) {
             modalRef.current?.showModal()
         }
-    },[showModal])
+    }, [showModal])
 
     return (
-        <dialog ref={modalRef} className="modal" onClose={() => showModalHandler(false, null)}>
+        <dialog ref={modalRef} className="modal" onClose={() => showModalHandler(false, null, null)}>
             <div className="modal-box bg-secondary text-secondary-content">
 
                 <p className="py-4">
-                    Are you sure you want to delete post&nbsp;
-                    <span className="font-bold">"{post?.title}"</span>
+                    Are you sure you want to delete the {currentEntity.entityType === 'post' ? 'post ' : 'user '}
+                    <span className="font-bold">
+                        "
+                        {
+                            currentEntity.entityType === 'post'
+                                ? (currentEntity.entity as IPost)?.title 
+                                : (currentEntity.entity as IUser)?.username}
+                        "
+                    </span>
                     &nbsp;?
                 </p>
 
