2436e129380d665a429e15e937227af36d4fc7be
Ariel Behar <ariel.behar@hotmail.com>
2023-10-05 20:39:18 +0300

    Modifying the AdminPosts component to display a DashBoardPost component instead of it being hard-coded

--- components/Admin/AdminPosts.tsx
+++ components/Admin/AdminPosts.tsx
@@ -1,11 +1,8 @@
 import { getPosts } from "@/lib/getPostData"
-import Image from "next/image";
 import { auth } from "@/lib/auth";
 import { Session } from "next-auth";
-import { ICustomSession } from "../Header/Navbar/NavLinks";
-import IconPencil from "../Icons/IconPencil";
-import Link from "next/link";
-import DeleteConfirmationModalButton from "../DeleteConfirmationModalButton";
+import DashboardPost from "../Dashboard/DashboardPost";
+import uniqid from "uniqid";
 
 async function AdminPosts() {
 	const posts = await getPosts();
@@ -16,24 +13,8 @@ async function AdminPosts() {
 			<h3>Posts</h3>
 
 			{posts.map((post) => (
-				<div key={post._id} className="my-5 flex items-center justify-between gap-5">
-					<Link href={`/blog/${post._id}`} className="flex items-center gap-5 hover:underline">
-						<Image src={post.img || "/img/noavatar.png"} alt="" width={50} height={50} />
-						<span className="hover:underline">{post.title}</span>
-					</Link>
-
-					{(session?.user?.id == post.userId || (session as ICustomSession).user?.isAdmin) && (
-						<div>
-							<button className="btn btn-sm btn-primary btn-outline border-none">
-								<IconPencil sizeClassName="size-5" />
-							</button>
-
-							<DeleteConfirmationModalButton entity={post} entityType='post'/>
-						</div>
-					)}
-				</div>
+				<DashboardPost key={uniqid()} post={post} session={session} />
 			))}
-
 		</div>
 	)
 }
