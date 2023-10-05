89379258ba191132e88a3b6cb53e7815ed499e5e
Ariel Behar <ariel.behar@hotmail.com>
2023-10-05 07:33:50 +0300

    Creating a FormStylesWrapper component to hold the styles of forms

---
+++ components/Forms/FormComponents/FormStylesWrapper.tsx
@@ -0,0 +1,21 @@
+
+interface Props {
+    title: string
+    classes?: string
+    children: React.ReactNode
+}
+
+function FormStylesWrapper({
+    title,
+    classes,
+    children
+}: Props) { 
+    return (
+        <div className={`w-[500px] bg-dark-soft p-3 text-center rounded-md ${classes}`}>
+            <h3 className="text-xl uppercase mb-3">{title}</h3>
+            {children}
+        </div>
+    )
+}
+
+export default FormStylesWrapper
