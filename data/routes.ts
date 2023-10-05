1867b1341f0c8be8ed1cffc5e346841ecbfb79b6
Ariel Behar <ariel.behar@hotmail.com>
2023-10-05 20:33:18 +0300

    Moving the routes array from the NavLinks to a separate data file

---
+++ data/routes.ts
@@ -0,0 +1,18 @@
+export const routes = [
+    {
+        title: 'Homepage',
+        path: '/'
+    },
+    {
+        title: 'About',
+        path: '/about'
+    },
+    {
+        title: 'Contact',
+        path: '/contact'
+    },
+    {
+        title: 'Blog',
+        path: '/blog'
+    }
+]
