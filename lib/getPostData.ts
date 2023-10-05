aae43088e770fa2265ab50efdf9d86a321975ed6
Ariel Behar <ariel.behar@hotmail.com>
2023-10-05 20:38:01 +0300

    Modifying the getPosts server action to be able to receive a userId as an argument

--- lib/getPostData.ts
+++ lib/getPostData.ts
@@ -2,32 +2,39 @@ import Post, { IPost } from "@/models/Post";
 import { IdType } from "@/types/common-types";
 import dbConnect from "./dbConnect";
 import { unstable_noStore as noStore } from "next/cache";
+import { IUser } from "@/models/User";
 
 export const getPost = async (postId: IdType) => {
     noStore();
-    
+
     try {
         dbConnect();
 
-        const post:IPost | null = await Post.findById(postId);
+        const post: IPost | null = await Post.findById(postId);
 
         return post;
-    } catch(error) {
+    } catch (error) {
         console.error(error);
         throw new Error('Failed to fetch post!');
     }
 }
 
-export const getPosts = async () => {
+export const getPosts = async (userId?: IUser['_id']) => {
     noStore();
 
     try {
         dbConnect();
 
-        const posts:IPost[] = await Post.find().lean();
+        let posts: IPost[];
+
+        if (userId) {
+           posts = await Post.find({userId}).lean();
+        } else {
+            posts = await Post.find().lean();
+        }
 
         return posts;
-    } catch(error) {
+    } catch (error) {
         console.error(error);
         throw new Error('Failed to fetch posts!');
     }
