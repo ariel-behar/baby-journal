b923a7c31b52c403815c7a28213d0dd12f6b5852
Ariel Behar <ariel.behar@hotmail.com>
2023-10-10 15:04:25 +0300

    Splitting the dashboard page into 2 sections - posts and user

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,23 @@
 import DashboardPosts from "@/components/Dashboard/DashboardPosts"
+import DashboardUser from "@/components/Dashboard/DashboardUser";
+import { auth } from "@/lib/auth"
+import { Session } from "next-auth"
 
-function DashboardPage() {
+async function DashboardPage() {
+	const session: Session | null = await auth();
+
+
 	return (
-		<div >
-			<DashboardPosts />
-		</div>
+		<>
+			<div className="flex justify-between">
+				<section>
+					<DashboardPosts />
+				</section>
+				<section>
+					<DashboardUser session={session} />
+				</section>
+			</div>
+		</>
 	)
 }
 
