14b5b8d4bc7ddda7bf60eef798ec88030b5ea0e7
Ariel Behar <ariel.behar@hotmail.com>
2023-10-10 15:03:55 +0300

    Creating a DashboardUser component where client's details and 'Add Post' button have been added

---
+++ components/Dashboard/DashboardUser.tsx
@@ -0,0 +1,24 @@
+import { ICustomSession } from "@/types/types";
+import { Session } from "next-auth"
+import AddPostModalButton from "../AddPostModalButton";
+
+interface Props {
+    session: Session | null
+}
+
+function DashboardUser({
+    session
+}: Props) {
+    const user = (session as ICustomSession)?.user;
+    
+
+    return (
+        <div>
+            <h3>{user?.firstName} {user?.lastName}</h3>
+
+            <AddPostModalButton />
+        </div>
+    )
+}
+
+export default DashboardUser
