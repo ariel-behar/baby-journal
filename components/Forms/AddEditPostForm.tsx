909b898e415ea4a49b0e3ddf527ffc15b665b2da
Ariel Behar <ariel.behar@hotmail.com>
2023-10-10 09:16:03 +0300

    Modifying the AddEditPostForm to fit for both adding/editing a post

--- components/Forms/AddEditPostForm.tsx
+++ components/Forms/AddEditPostForm.tsx
@@ -3,7 +3,7 @@ import { yupResolver } from "@hookform/resolvers/yup"
 import { BaseSyntheticEvent } from "react"
 import { useForm } from "react-hook-form"
 
-import { addPost } from "@/lib/serverActions"
+import { addPost, editPost } from "@/lib/serverActions"
 import { IUser } from "@/models/User"
 import postSchema from "@/validation/postSchema"
 
@@ -21,25 +21,25 @@ export interface IPostFormData {
 interface Props {
     userId: IUser['_id']
     formType: 'add' | 'edit'
-    post?: IPost
+    post?: IPost,
+    modalRef?: React.RefObject<HTMLDialogElement>
 }
 
 function AddEditPostForm({
     userId,
     formType,
-    post
+    post,
+    modalRef
 }: Props) {
-    const defaultValues = {
-        title: (formType === 'edit' && post?.title) || '',
-        description: (formType === 'edit' && post?.description) || '',
-        img: (formType === 'edit' && post?.img) || '',
-        userId: userId
-    }
-
     const { register, handleSubmit, formState: { errors, isValid, isDirty } } = useForm<IPostFormData>({
         resolver: yupResolver(postSchema),
         mode: 'onBlur',
-        defaultValues
+        defaultValues: {
+            title: (formType === 'edit' && post?.title) || '',
+            description: (formType === 'edit' && post?.description) || '',
+            img: (formType === 'edit' && post?.img) || '',
+            userId: userId
+        }
     });
 
     const onFormSubmit = async (formData: IPostFormData, e: BaseSyntheticEvent<object, any, any> | undefined) => {
@@ -48,25 +48,47 @@ function AddEditPostForm({
         const { title, description, img, userId } = formData;
 
         if (title && description && img && userId) {
-            try {
-                const response = await addPost({ title, description, img, userId });
-                console.log('response:', response)
+            if (formType === 'add') {
+                try {
+                    const response = await addPost({ title, description, img, userId });
+                    console.log('response:', response)
 
-            } catch (error) {
-                console.log(error);
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                try {
+                    const response = await editPost(post?._id as string, { title, description, img, userId });
+                    console.log('response:', response)
+
+                    modalRef?.current?.close();
+                } catch (error) {
+                    console.log(error);
+                }
             }
+
         }
     }
 
     return (
-        <form onSubmit={handleSubmit(onFormSubmit)} className="flex flex-col gap-3">
-            <FormInputFieldWithTooltip register={register} errors={errors} name="title" label='Title' type='text' />
-            <FormInputFieldWithTooltip register={register} errors={errors} name="description" label='Description' type='text' />
-            <FormInputFieldWithTooltip register={register} errors={errors} name="img" label='Image' type='text' />
-            <input type="hidden" {...register('userId')} value={userId} name="userId" />
+        <>
+            <form onSubmit={handleSubmit(onFormSubmit)} className="flex flex-col gap-3">
+                <FormInputFieldWithTooltip register={register} errors={errors} name="title" label='Title' type='text' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="description" label='Description' type='text' />
+                <FormInputFieldWithTooltip register={register} errors={errors} name="img" label='Image' type='text' />
+                <input type="hidden" {...register('userId')} value={userId} name="userId" />
 
-            <FormSubmitButton isDirty={isDirty} isValid={isValid}>{formType === 'edit' ? 'Edit' : 'Add'} Post</FormSubmitButton>
-        </form>
+                <div className="w-full flex justify-around mt-2">
+                    {formType === 'edit' && (
+                        <button type="button" className="btn btn-error btn-lg" onClick={() => modalRef?.current?.close()}>Cancel</button>
+                    )}
+
+                    <FormSubmitButton isDirty={isDirty} isValid={isValid}>{formType === 'edit' ? 'Edit' : 'Add'} Post</FormSubmitButton>
+                </div>
+            </form>
+
+
+        </>
     )
 }
 
