a0fde95b5d7ec7f57999532f022786ea3a4c329d
Ariel Behar <ariel.behar@hotmail.com>
2023-10-10 09:14:14 +0300

    Creating the PostEditModal component and its ref to the AddEditPostForm

--- components/Modal/PostEditModal.tsx
+++ components/Modal/PostEditModal.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from "react"
 import AddEditPostForm from "../Forms/AddEditPostForm"
 import { Session } from "next-auth"
 import { IPost } from "@/models/Post"
+import FormStylesWrapper from "../Forms/FormComponents/FormStylesWrapper"
 
 interface Props {
     session: Session | null
@@ -12,7 +13,7 @@ interface Props {
 function PostEditModal({
     session
 }: Props) {
-    const { modalSettings: { showModal }, currentEntity: {entity}, showModalHandler } = useModalContext()
+    const { modalSettings: { showModal }, currentEntity: { entity }, showModalHandler } = useModalContext()
     const modalRef = useRef<HTMLDialogElement | null>(null)
 
     useEffect(() => {
@@ -23,33 +24,12 @@ function PostEditModal({
 
     return (
         <dialog ref={modalRef} className="modal" onClose={() => showModalHandler(false, null, null, null)}>
-            <div className="modal-box bg-secondary text-secondary-content">
-
-                {/* <p className="py-4">
-                    Are you sure you want to delete the {currentEntity.entityType === 'post' ? 'post ' : 'user '}
-                    <span className="font-bold">
-                        "
-                        {
-                            currentEntity.entityType === 'post'
-                                ? (currentEntity.entity as IPost)?.title 
-                                : (currentEntity.entity as IUser)?.username}
-                        "
-                    </span>
-                    &nbsp;?
-                </p>
-
-                <div className="modal-action">
-                    <form method="dialog" className="flex justify-center w-full gap-1 sm:gap-10"> */}
-                {/* if there is a button in form, it will close the modal */}
-                {/* <button className="btn btn-sm btn-error btn-outline" onClick={() => deletePostHandler(true)}>
-                            Delete Post <IconTrash sizeClassName="size-5" />
-                        </button>
-
-                        <button className="btn btn-sm btn-primary sm:px-7" onClick={() => deletePostHandler(false)}>Cancel</button>
-                    </form>
-                </div> */}
-
-                <AddEditPostForm userId={session?.user?.id as string} formType="edit" post={entity as IPost}  />
+            <div className="modal-box bg-dark-soft">
+                <div className="modal-action mt-0">
+                    <FormStylesWrapper title="Edit Post">
+                        <AddEditPostForm userId={session?.user?.id as string} formType="edit" post={entity as IPost} modalRef={modalRef} />
+                    </FormStylesWrapper>
+                </div>
             </div>
         </dialog>
     )
