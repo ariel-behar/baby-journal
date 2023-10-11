73fb71fd12e1bac47ea63b5d3f09fd64debcfc17
Ariel Behar <ariel.behar@hotmail.com>
2023-10-11 18:01:36 +0300

    Changing 'classes' to 'className' in the FormStylesWrapper

--- components/Forms/FormComponents/FormStylesWrapper.tsx
+++ components/Forms/FormComponents/FormStylesWrapper.tsx
@@ -1,17 +1,17 @@
 
 interface Props {
     title: string
-    classes?: string
+    className?: string
     children: React.ReactNode
 }
 
 function FormStylesWrapper({
     title,
-    classes,
+    className = '',
     children
 }: Props) { 
     return (
-        <div className={`bg-dark-soft p-3 text-center rounded-md ${classes}`}>
+        <div className={`bg-dark-soft p-3 text-center rounded-md ${className}`}>
             <h3 className="text-xl uppercase mb-3">{title}</h3>
             {children}
         </div>
