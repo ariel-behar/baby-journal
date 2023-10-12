fd92983a4c02ee9059cd56da542cf4299e758e7f
Ariel Behar <ariel.behar@hotmail.com>
2023-10-12 20:41:08 +0300

    Changing the AddPostModalButton text to 'Add New Post'

--- components/Buttons/AddPostModalButton.tsx
+++ components/Buttons/AddPostModalButton.tsx
@@ -8,7 +8,7 @@ function AddPostModalButton() {
             className="btn btn-sm btn-primary btn-min-width"
             onClick={() => showModalHandler(true, 'add', null, 'post')}
         >
-            Add Post
+            Add New Post
         </button>
     )
 }
