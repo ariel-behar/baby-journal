ab42ba38bbce54f14c5c6566e256fea305e0e678
Ariel Behar <ariel.behar@hotmail.com>
2023-10-14 10:37:41 +0300

    Creating the TableRow component

---
+++ components/Table/TableRow.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react"
+
+interface Props{
+    children: ReactNode,
+    index: number
+}
+
+function TableRow({
+    children,
+    index
+}: Props) {
+    return (
+        <tr className={`${index % 2 === 0 ? 'bg-gray-900/30' : ''}`}>
+            {children}
+        </tr>
+    )
+}
+
+export default TableRow
