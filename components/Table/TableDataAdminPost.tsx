ee5a3b5361322553ec051817377d2ef06abdec08
Ariel Behar <ariel.behar@hotmail.com>
2023-10-15 10:48:31 +0300

    Creating the TableDataAdminPost component

---
+++ components/Table/TableDataAdminPost.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import TableDataPost from './TableDataPost'
+import { IPostPopulated } from '@/models/Post'
+import { Session } from 'next-auth'
+
+interface Props {
+    post: IPostPopulated,
+    session: Session | null
+}
+
+function TableDataAdminPost({
+    post,
+    session,
+}: Props) {
+    return (
+        <TableDataPost post={post} session={session}>
+            {/* Username */}
+            <td>
+                {post.userId.username}
+            </td>
+
+            {/* Email */}
+            <td>
+                {post.userId.email}
+            </td>
+
+        </TableDataPost>
+    )
+}
+
+export default TableDataAdminPost
