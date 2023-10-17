ae99bf8d67886f13dad4b0e56952608ffe970417
Ariel Behar <ariel.behar@hotmail.com>
2023-10-17 19:05:09 +0300

    Adding the IconPlus on the AddPostModalButton

--- components/Buttons/AddPostModalButton.tsx
+++ components/Buttons/AddPostModalButton.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useModalContext } from "@/context/modalContext"
 
+import IconPlus from "../Icons/IconPlus"
+
 function AddPostModalButton() {
     const { showModalHandler } = useModalContext()
     return (
@@ -8,7 +10,7 @@ function AddPostModalButton() {
             className="btn btn-sm btn-primary btn-min-width"
             onClick={() => showModalHandler(true, 'add', null, 'post')}
         >
-            Add New Post
+            Add New Post <IconPlus sizeClassName="size-5" />
         </button>
     )
 }
