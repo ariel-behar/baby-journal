1ba46526dab7b2fb45d47935a936a644cf00dd27
Ariel Behar <ariel.behar@hotmail.com>
2023-10-19 22:30:16 +0300

    Adding a hover effect to the nav links

--- components/Header/Navbar/NavLink.tsx
+++ components/Header/Navbar/NavLink.tsx
@@ -19,9 +19,10 @@ function NavLink({
 
     return (
         <Link
-            className={`min-w-24 px-3 py-2 rounded-2xl font-bold text-center ${pathname === path
-                ? 'bg-light text-dark'
-                : ''}
+            className={`min-w-24 px-3 py-2 rounded-2xl font-bold text-center hover:bg-light hover:text-dark 
+                ${pathname === path
+                    ? 'bg-light text-dark'
+                    : ''}
                 `}
             key={uniqid()}
             href={path}
