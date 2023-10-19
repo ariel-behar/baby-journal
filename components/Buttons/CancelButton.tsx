eeb06ab528da84f168f5897b34e239d71a3ae236
Ariel Behar <ariel.behar@hotmail.com>
2023-10-19 22:42:03 +0300

    Adding a close icon in the cancel button

--- components/Buttons/CancelButton.tsx
+++ components/Buttons/CancelButton.tsx
@@ -1,16 +1,19 @@
+import IconClose from "../Icons/IconClose"
 
 interface Props {
-    className?: string
-    onClick?: () => void
+	className?: string
+	onClick?: () => void
 }
 
 function CancelButton({
-    className = "btn-error",
-    onClick,
-}:Props) {
-  return (
-    <button type="button" className={`btn ${className} btn-sm btn-min-width`} onClick={onClick}>Cancel</button>
-  )
+	className = "btn-error",
+	onClick,
+}: Props) {
+	return (
+		<button type="button" className={`btn ${className} btn-sm btn-min-width`} onClick={onClick}>
+			Cancel <IconClose sizeClassName="size-5" />
+		</button>
+	)
 }
 
 export default CancelButton
