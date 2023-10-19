0c1353dd95fc06c56464461387060a3df1202511
Ariel Behar <ariel.behar@hotmail.com>
2023-10-19 22:49:55 +0300

    Creating the IconChevronRight component

---
+++ components/Icons/IconChevronRight.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    sizeClassName?: "size-1" | "size-2" | "size-3" | "size-4" | "size-5" | "size-6" | "size-7" | "size-8" | "size-9" | "size-10"
+}
+function IconChevronRight({
+    sizeClassName = 'size-6'
+}: Props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={sizeClassName}>
+            <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+        </svg>
+
+    )
+}
+
+export default IconChevronRight
