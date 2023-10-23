00022fde03032570b80956dadf365432661861d1
Ariel Behar <ariel.behar@hotmail.com>
2023-10-23 07:58:15 +0300

    Renaming the PostCard component to BlogPostCard in the blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 import { IPostPopulated } from "@/models/Post";
 import { getPosts } from "@/lib/getPostData";
 
-import PostCard from "@/components/Blog/BlogPostCard"
+import BlogPostCard from "@/components/Blog/BlogPostCard"
 import { auth } from "@/lib/auth";
 import { Session } from "next-auth";
 import { ICustomSession } from "@/types/types";
@@ -25,7 +25,7 @@ async function BlogPage() {
 		<div className="py-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 gap-y-5 lg:gap-5">
 			{
 				posts.map((post) => (
-					<PostCard user={user} post={post} key={uniqid()}  />
+					<BlogPostCard user={user} post={post} key={uniqid()}  />
 				))
 			}
 		</div>
