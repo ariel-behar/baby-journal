6c8d25b7dd2a7f875599d894a308754e29de2e03
Ariel Behar <ariel.behar@hotmail.com>
2023-10-23 07:57:20 +0300

    Fixing the bug in the like button that was not comparing user ids properly

--- components/Buttons/LikeButton.tsx
+++ components/Buttons/LikeButton.tsx
@@ -15,15 +15,16 @@ function LikeButton({
     user,
     post
 }: Props) {
+    const stringifiedUserLikeIds = post.likes.map(like => like.toString());
 
     return (
         <>
-            <form action={post.likes.includes(user?.id as string) ? unlikePost : likePost}>
+            <form action={stringifiedUserLikeIds.includes(user?.id as string) ? unlikePost : likePost}>
                 <input type="hidden" name="userId" value={user?.id} />
                 <input type="hidden" name="postId" value={post._id} />
                 <button className='transform hover:scale-110 duration-200'>
                     {
-                        post.likes.includes(user?.id as string)
+                        stringifiedUserLikeIds.includes(user?.id as string)
                             ? <IconHeartSolid fillColor='#D32F2F' sizeClassName='size-6' />
                             : <IconHeartOutline sizeClassName='size-6' />
                     }
