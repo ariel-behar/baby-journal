7fc6dc2a428cae8b4d740efd372ce2f5d02e1d70
Ariel Behar <ariel.behar@hotmail.com>
2023-10-24 09:48:34 +0300

    Fixing the layout of the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,16 +4,14 @@ import DashboardPosts from "@/components/Dashboard/DashboardPosts"
 
 async function DashboardPage() {
 	return (
-		<>
-			<section>
-				<div className="flex justify-between mt-5 mb-3">
-					<h3 className="text-xl uppercase text-center">Posts</h3>
-					<AddPostModalButton />
-				</div>
-				
-				<DashboardPosts />
-			</section>
-		</>
+		<section className="flex-grow">
+			<div className="flex justify-between mt-5 mb-3">
+				<h3 className="text-xl uppercase text-center">Posts</h3>
+				<AddPostModalButton />
+			</div>
+
+			<DashboardPosts />
+		</section>
 	)
 }
 
