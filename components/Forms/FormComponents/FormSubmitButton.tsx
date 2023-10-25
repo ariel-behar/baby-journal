10f6215656b55fe4b32230df014af8386bd5ec6a
Ariel Behar <ariel.behar@hotmail.com>
2023-10-25 10:45:37 +0300

    Adding a 'className' prop to the FormSubmitButton component

--- components/Forms/FormComponents/FormSubmitButton.tsx
+++ components/Forms/FormComponents/FormSubmitButton.tsx
@@ -2,17 +2,19 @@
 interface Props {
     isDirty: boolean;
     isValid: boolean;
+    className?: string;
     children: React.ReactNode;
 }
 
 function FormSubmitButton({
     isDirty,
     isValid,
+    className,
     children
 }: Props) {
     return (
         <button
-            className="btn btn-sm btn-primary btn-min-width disabled:bg-primary disabled:opacity-30 disabled:text-primary-content"
+            className={`btn btn-primary btn-min-width disabled:bg-primary disabled:opacity-30 disabled:text-primary-content ${className}`}
             disabled={!(isDirty && isValid)}
             type='submit'
         >
