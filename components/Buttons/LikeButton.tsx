8cebba6163bea2b89ad6a09a68bb1c3a34b8a473
Ariel Behar <ariel.behar@hotmail.com>
2023-10-27 14:02:06 +0300

    Modifiying the functionality of the LikeButton, so it displays a toast when liking/unliking a post

--- components/Buttons/LikeButton.tsx
+++ components/Buttons/LikeButton.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useToastContext } from '@/context/toastContext'
 import { IPost, IPostPopulated } from '@/models/Post'
 import { ICustomSession } from '@/types/types'
 
@@ -16,21 +18,34 @@ function LikeButton({
     post
 }: Props) {
     const stringifiedUserLikeIds = post.likes.map(like => like.toString());
+    const { showToast } = useToastContext();
+
+    const formHandler = () => {
+        if (stringifiedUserLikeIds.includes(user?.id as string)) {
+            unlikePost(user?.id as string, post._id)
+                .then(res => {
+                    if (res.ok) {
+                        showToast(res.message, 'error')
+                    }
+                })
+        } else {
+            likePost(user?.id as string, post._id)
+                .then(res => {
+                    if (res.ok) {
+                        showToast(res.message, 'success')
+                    }
+                })
+        }
+    }
 
     return (
-        <>
-            <form action={stringifiedUserLikeIds.includes(user?.id as string) ? unlikePost : likePost}>
-                <input type="hidden" name="user" value={user?.id} />
-                <input type="hidden" name="postId" value={post._id} />
-                <button className='transform hover:scale-110 duration-200'>
-                    {
-                        stringifiedUserLikeIds.includes(user?.id as string)
-                            ? <IconHeartSolid fillColor='#D32F2F' sizeClassName='size-6' />
-                            : <IconHeartOutline sizeClassName='size-6' />
-                    }
-                </button>
-            </form>
-        </>
+        <button className='transform hover:scale-110 duration-200' onClick={formHandler}>
+            {
+                stringifiedUserLikeIds.includes(user?.id as string)
+                    ? <IconHeartSolid fillColor='#D32F2F' sizeClassName='size-6' />
+                    : <IconHeartOutline sizeClassName='size-6' />
+            }
+        </button>
     )
 }
 
