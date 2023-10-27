aa2d27e1163856d4cf3b290ef5caac8b62af33dd
Ariel Behar <ariel.behar@hotmail.com>
2023-10-27 14:19:42 +0300

    Renaming and restructuring the toastContext to notificationContext

---
+++ context/notificationContext.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { createContext, useContext, useState } from 'react';
+
+export type TNotificationType = 'success' | 'error' | 'info' | 'warning';
+
+interface INotification {
+    message: string,
+    type: TNotificationType,
+    options?: any
+}
+
+interface INotificationContext {
+    notification: INotification;
+    displayNotification: (message: string, type: TNotificationType, options?: any) => void;
+}
+
+const initialNotificationState: INotification = {
+    message: '',
+    type: 'info',
+    options: {}
+}
+
+export const NotificationContext = createContext<INotificationContext>({
+    notification: initialNotificationState,
+    displayNotification: () => { }
+});
+
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export const NotificationContextProvider = ({
+    children
+}: Props) => {
+    const [notification, setNotification] = useState<INotification>(initialNotificationState);
+
+    function displayNotification(message: string, type: TNotificationType, options?: any) {
+        setNotification(prevState => {
+            return {
+                ...prevState,
+                message, 
+                type, 
+                options 
+            }
+        });
+    }
+
+    return (
+        <NotificationContext.Provider value={{ notification, displayNotification }}>
+            {children}
+        </NotificationContext.Provider>
+    )
+}
+
+export const useNotificationContext = () => {
+    return useContext(NotificationContext);
+}
