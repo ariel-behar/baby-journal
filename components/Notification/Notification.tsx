bbea9d551518cd91e2c64aa03174593594030e3d
Ariel Behar <ariel.behar@hotmail.com>
2023-10-27 14:20:16 +0300

    Renaming and restructuring the Toast component to Notification

---
+++ components/Notification/Notification.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { TNotificationType, useNotificationContext } from "@/context/notificationContext";
+import { useEffect } from "react";
+import { ToastContainer, toast, Flip } from "react-toastify"
+
+
+function Notification() {
+    const { notification } = useNotificationContext()
+
+    useEffect(() => {
+        const { message, type, options } = notification
+
+        if (message) {
+            toast[`${(type as TNotificationType)}`](message, options)
+        }
+    }, [notification])
+
+
+    return (
+        <ToastContainer
+            position="bottom-left"
+            autoClose={1500}
+            limit={5}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable={false}
+            pauseOnHover
+            theme="dark"
+            transition={Flip}
+        />
+    )
+}
+
+export default Notification
