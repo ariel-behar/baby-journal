60d8835cb95f550df2b3d3ec1b8e0ac01bd4b47c
Ariel Behar <ariel.behar@hotmail.com>
2023-10-28 15:33:16 +0300

    Displaying a notification on success when the DeleteButton is clicked

--- components/Buttons/DeleteButton.tsx
+++ components/Buttons/DeleteButton.tsx
@@ -1,16 +1,35 @@
 "use client"
+import { useSession } from 'next-auth/react'
+
+import { useNotificationContext } from '@/context/notificationContext'
 import { useModalContext } from '@/context/modalContext'
 
 import IconTrash from '../Icons/IconTrash'
-import { useSession } from 'next-auth/react'
 
 function DeleteButton() {
     const { deletePostHandler } = useModalContext()
+    const { displayNotification } = useNotificationContext();
     const session = useSession()
     const currentUserId = session.data?.user?.id
-    
+
+    const onDeleteButtonClick = () => {
+        try {
+            const deleteResponse = deletePostHandler(true, currentUserId as string)
+
+            if (deleteResponse) {
+                deleteResponse.then(res => {
+                    if (res.ok) {
+                        displayNotification(res.message, 'info')
+                    }
+                })
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <button className="btn btn-sm btn-error btn-min-width" onClick={() => deletePostHandler(true, currentUserId as string)}>
+        <button className="btn btn-sm btn-error btn-min-width" onClick={onDeleteButtonClick}>
             Delete <IconTrash />
         </button>
     )
