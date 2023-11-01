119d03ab15cfb471868b44322c77b7d2038c2489
Ariel Behar <ariel.behar@hotmail.com>
2023-11-01 13:29:51 +0200

    LoginForm is modified to display notifications for errors and successful log in

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -1,9 +1,13 @@
 "use client"
-import { loginUser } from '@/lib/serverActions';
-import { userLoginSchema } from '@/validation/userSchema';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { BaseSyntheticEvent } from 'react';
 import { useForm } from 'react-hook-form';
+
+import { loginUser } from '@/lib/serverActions';
+import { userLoginSchema } from '@/validation/userSchema';
+
+import { useNotificationContext } from '@/context/notificationContext';
+
 import FormInputFieldWithTooltip from './FormComponents/FormInputFieldWithTooltip';
 import FormSubmitButton from './FormComponents/FormSubmitButton';
 
@@ -13,6 +17,7 @@ export interface ILoginFormData {
 }
 
 function LoginForm() {
+    const { displayNotification } = useNotificationContext();
     const { register, handleSubmit, formState: { isDirty, isValid, errors } } = useForm<ILoginFormData>({
         resolver: yupResolver(userLoginSchema),
         mode: 'onBlur',
@@ -28,7 +33,12 @@ function LoginForm() {
         const { username, password } = formData;
 
         if (username && password) {
-            await loginUser(formData);
+            await loginUser(formData)
+                .then(res => {
+                    displayNotification('Successfully logged in!', 'success')
+                }).catch(error => {
+                    displayNotification(error.message, 'error')
+                })
         }
     }
 
