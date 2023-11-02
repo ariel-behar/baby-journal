5a42210ec101fe02d071a8dd07206e6cc1dee9e7
Ariel Behar <ariel.behar@hotmail.com>
2023-11-02 17:21:00 +0200

    Adding a NotFoundError to the getPostData services

--- lib/getPostData.ts
+++ lib/getPostData.ts
@@ -5,6 +5,8 @@ import { unstable_noStore as noStore } from "next/cache";
 import { IUser } from "@/models/User";
 import mongoose from "mongoose";
 import { redirect } from "next/navigation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { NotFoundError } from "@/models/Error";
 
 export const getPost = async (postId: IdType) => {
     noStore();
@@ -15,12 +17,19 @@ export const getPost = async (postId: IdType) => {
         if (mongoose.Types.ObjectId.isValid(postId)) {
             const post: IPost | null = await Post.findById(postId);
 
+            if(!post) {
+                throw new NotFoundError('Post not found!')
+            }
+
             return post;
         } else {
             redirect('/blog');
         }
 
     } catch (error) {
+        if(isRedirectError(error)){
+            redirect('/blog');
+        }
         throw error
     }
 }
@@ -46,13 +55,15 @@ export const getPosts = async (populateUser: boolean, userId?: IUser['_id']) => 
             } else {
                 posts = await Post.find().lean();
             }
+        }
 
+        if(!posts){
+            throw new NotFoundError('Failed to fetch posts!')
         }
 
         return posts;
     } catch (error) {
-        console.error(error);
-        throw new Error('Failed to fetch posts!');
+        throw error;
     }
 }
 
