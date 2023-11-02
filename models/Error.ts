cf3ee910717b60d0880e3f61df17565e86d0f8eb
Ariel Behar <ariel.behar@hotmail.com>
2023-11-02 17:10:24 +0200

    Implementing a new InvalidDataError in the Error model

--- models/Error.ts
+++ models/Error.ts
@@ -4,4 +4,8 @@ export class InvalidLoginError extends CredentialsSignin {
     message = "Invalid login credentials!";
 }
 
+export class InvalidDataError extends Error {
+}
+
 export class NotFoundError extends Error {}
+
