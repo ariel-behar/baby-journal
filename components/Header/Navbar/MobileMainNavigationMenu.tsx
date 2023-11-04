ccbbdaa92f31f1308f02a518e88964da6e350b60
Ariel Behar <ariel.behar@hotmail.com>
2023-11-04 08:54:47 +0200

    Changing the mobile menu design and adding a menu toggler to various buttons/elements

--- components/Header/Navbar/MobileMainNavigationMenu.tsx
+++ components/Header/Navbar/MobileMainNavigationMenu.tsx
@@ -6,6 +6,7 @@ import { IRoute } from '@/types/types';
 
 import NavLink from "./NavLink"
 import IconMenu from '@/components/Icons/IconMenu';
+import IconClose from '@/components/Icons/IconClose';
 
 interface Props {
     routes: IRoute[]
@@ -14,25 +15,39 @@ interface Props {
 function MobileMainNavigationMenu({
     routes
 }: Props) {
-    const [open, setOpen] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    function toggleMenu() {
+        setIsMenuOpen((prev) => !prev)
+    }
+
     return (
         <>
             {/* Mobile Menu */}
-            <span className='block lg:hidden' onClick={() => setOpen((prev) => !prev)}>
+            <span className='block lg:hidden' onClick={toggleMenu}>
                 <IconMenu sizeClassName='size-10' />
             </span >
 
 
             {
-                open && (
-                    <div onMouseOut={() => setOpen((prev) => false)} className='lg:hidden flex flex-col items-center justify-center absolute top-[100px] right-0 w-1/2 h-[calc(100vh-100px)] bg-green-500 gap-3 z-10'>
-                        {
-                            routes.map((link) => {
-                                return (
-                                    <NavLink key={uniqid()} {...link} />
-                                )
-                            })
-                        }
+                isMenuOpen && (
+                    <div className='absolute top-0 left-0 h-screen w-screen'>
+                        <div className='absolute z-10 top-0 left-0 h-screen w-screen backdrop-brightness-[30%]' onClick={toggleMenu}>
+                        </div>
+
+                        <div className='lg:hidden flex flex-col items-center justify-center absolute top-0 right-0 w-3/4 h-screen bg-green-500 gap-3 z-10'>
+                            <span className='absolute top-3 right-3' onClick={toggleMenu}>
+                                <IconClose sizeClassName='size-8' />
+                            </span>
+                            {
+                                routes.map((link) => {
+                                    return (
+                                        <NavLink key={uniqid()} {...link} toggleMenu={toggleMenu} />
+                                    )
+                                })
+                            }
+                        </div>
+
                     </div>
                 )
             }
