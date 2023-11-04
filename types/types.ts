92f24b9625596d4dcde3f22d36f60e40f1a6b643
Ariel Behar <ariel.behar@hotmail.com>
2023-11-04 06:30:14 +0200

    Creating an IRoute interface in the types file

--- types/types.ts
+++ types/types.ts
@@ -12,3 +12,8 @@ export interface ICustomSession extends Session {
 export interface IIconProps {
     sizeClassName?: "size-1" | "size-2" | "size-3" | "size-4" | "size-5" | "size-6" | "size-7" | "size-8" | "size-9" | "size-10"
 }
+
+export interface IRoute {
+    title: string,
+    path: string
+}
