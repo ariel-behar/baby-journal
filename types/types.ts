74fe700f2495f0356bc5e4eef486d5ab09d0bc88
Ariel Behar <ariel.behar@hotmail.com>
2023-11-04 06:14:16 +0200

    Creating the IIconProps interface in the types file

--- types/types.ts
+++ types/types.ts
@@ -8,3 +8,7 @@ export interface ICustomSession extends Session {
         isAdmin: boolean
     }
 }
+
+export interface IIconProps {
+    sizeClassName?: "size-1" | "size-2" | "size-3" | "size-4" | "size-5" | "size-6" | "size-7" | "size-8" | "size-9" | "size-10"
+}
