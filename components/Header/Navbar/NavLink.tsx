a044b486c2ce231a0839e1caacbe9ed79586a5ef
Ariel Behar <ariel.behar@hotmail.com>
2023-11-04 06:40:36 +0200

    Modifying the width and margins of the NavLink to be only on large screens

--- components/Header/Navbar/NavLink.tsx
+++ components/Header/Navbar/NavLink.tsx
@@ -18,7 +18,7 @@ function NavLink({
 
     return (
         <Link
-            className={`min-w-24 mx-3 px-3 py-2 rounded-2xl font-bold text-center hover:bg-light hover:text-dark 
+            className={`lg:min-w-24 lg:mx-3 px-3 py-2 rounded-2xl font-bold text-center hover:bg-light hover:text-dark 
                 ${pathname === path
                     ? 'bg-light text-dark'
                     : ''}
