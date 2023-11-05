4361b5354607811e86831445fcbd9f024a6b93ae
Ariel Behar <ariel.behar@hotmail.com>
2023-11-05 05:30:11 +0200

    Creating a sliding drawer animation for the mobile menu

--- components/Header/Navbar/MobileMainNavigationMenu.tsx
+++ components/Header/Navbar/MobileMainNavigationMenu.tsx
@@ -23,34 +23,38 @@ function MobileMainNavigationMenu({
 
     return (
         <>
-            {/* Mobile Menu */}
+            {/* Hamburger Icon */}
             <span className='block lg:hidden' onClick={toggleMenu}>
                 <IconMenu sizeClassName='size-10' />
             </span >
 
 
-            {
-                isMenuOpen && (
-                    <div className='absolute top-0 left-0 h-screen w-screen'>
-                        <div className='absolute z-10 top-0 left-0 h-screen w-screen backdrop-brightness-[30%]' onClick={toggleMenu}>
-                        </div>
+            <div className={`${isMenuOpen ? 'visible' : 'invisible'} absolute top-0 right-0 h-screen w-screen`}>
+                {/* Backdrop */}
+                <div className={`${isMenuOpen ? 'backdrop-brightness-[30%]' : 'backdrop-brightness-[100%]'} absolute z-10 top-0 left-0 h-screen w-screen transition-all duration-1000`} onClick={toggleMenu}>
+                </div>
 
-                        <div className='lg:hidden flex flex-col items-center justify-center absolute top-0 right-0 w-3/4 h-screen bg-green-500 gap-3 z-10'>
-                            <span className='absolute top-3 right-3' onClick={toggleMenu}>
-                                <IconClose sizeClassName='size-8' />
-                            </span>
-                            {
-                                routes.map((link) => {
-                                    return (
-                                        <NavLink key={uniqid()} {...link} toggleMenu={toggleMenu} />
-                                    )
-                                })
-                            }
-                        </div>
+                {/* Menu */}
+                <div className={`${isMenuOpen ? 'w-3/4' : 'w-0'} lg:hidden absolute top-0 right-0  h-screen bg-green-500 gap-3 z-10 transition-all duration-1000 overflow-hidden `}>
 
+                    {/* Close Menu */}
+                    <span className={`${isMenuOpen ? 'opacity-100' : 'opacity-0'} absolute top-3 right-3 transition-all duration-1000`} onClick={toggleMenu}>
+                        <IconClose sizeClassName='size-9'  />
+                    </span>
+
+                    {/* Links */}
+                    <div className={`${isMenuOpen ? 'opacity-100' : 'opacity-0'} h-screen flex flex-col items-center justify-center transition-all duration-1000 `}>
+                        {
+                            routes.map((link) => {
+                                return (
+                                    <NavLink key={uniqid()} {...link} toggleMenu={toggleMenu} />
+                                )
+                            })
+                        }
                     </div>
-                )
-            }
+                </div>
+
+            </div>
         </>
     )
 }
