18b98be3fabaeac93813d14b63b10888ec9bb0b7
Ariel Behar <ariel.behar@hotmail.com>
2023-11-07 09:05:21 +0200

    Creating the LanguageSelector component

---
+++ components/LanguageSelector.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { ChangeEvent, useTransition } from "react"
+import { useLocale } from "next-intl";
+import uniqid from "uniqid";
+
+import { useRouter } from "@/lib/i18nNavigation";
+import { localesWithLabels } from "@/i18n";
+
+function LanguageSelector() {
+    const [isPending, startTransition] = useTransition()
+    const currentLocale = useLocale()
+    const router = useRouter();
+
+    function handleChange(e: ChangeEvent<HTMLSelectElement>) {
+        const nextLocale = e.target.value;
+
+        startTransition(() => {
+            router.replace(`/`, { locale: nextLocale })
+        });
+    }
+
+    return (
+        <select defaultValue='en' onChange={handleChange} value={currentLocale} disabled={isPending}>
+            {
+                localesWithLabels.map(locale => (
+                    <option key={uniqid()} value={locale.locale}>{locale.label}</option>
+                ))
+            }
+        </select>
+    )
+}
+
+export default LanguageSelector
