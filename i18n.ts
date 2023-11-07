557a22dca4237c9be99c88ae5d86bac49b092940
Ariel Behar <ariel.behar@hotmail.com>
2023-11-07 09:04:27 +0200

    Creating a localesWithLabels variable in the i18n.ts file

--- i18n.ts
+++ i18n.ts
@@ -1,13 +1,17 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
- 
+import { notFound } from 'next/navigation';
+import { getRequestConfig } from 'next-intl/server';
+
 // Can be imported from a shared config
 export const locales = ['en', 'es'];
- 
-export default getRequestConfig(async ({locale}) => {
+export const localesWithLabels = [
+  { locale: 'en', label: 'English' },
+  { locale: 'es', label: 'Español' },
+];
+
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
- 
+  if (!locales.includes(locale as string)) notFound();
+
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
