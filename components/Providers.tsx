4687a509e1dbd304503ea1c13cf4b8be02c2812d
Ariel Behar <ariel.behar@hotmail.com>
2023-11-07 08:52:20 +0200

    Including the NextIntlClientProvider in the Providers component

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,22 +1,27 @@
 import { ModalContextProvider } from '@/context/modalContext'
 import { NotificationContextProvider } from '@/context/notificationContext'
 import { SessionProvider } from 'next-auth/react'
+import { AbstractIntlMessages, NextIntlClientProvider } from 'next-intl'
 
 interface Props {
     children: React.ReactNode
+    messages: AbstractIntlMessages | undefined
 }
 
 function Providers({
-    children
+    children,
+    messages
 }: Props) {
     return (
-        <SessionProvider>
-            <NotificationContextProvider>
-                <ModalContextProvider>
-                    {children}
-                </ModalContextProvider>
-            </NotificationContextProvider>
-        </SessionProvider>
+        <NextIntlClientProvider messages={messages as AbstractIntlMessages}>
+            <SessionProvider>
+                <NotificationContextProvider>
+                    <ModalContextProvider>
+                        {children}
+                    </ModalContextProvider>
+                </NotificationContextProvider>
+            </SessionProvider>
+        </NextIntlClientProvider>
     )
 }
 
