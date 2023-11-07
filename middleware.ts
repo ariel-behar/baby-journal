69cfa8d1419c15fe99a71047cb53ae7fc873191b
Ariel Behar <ariel.behar@hotmail.com>
2023-11-07 18:16:12 +0200

    modifying the middleware.ts locales

--- middleware.ts
+++ middleware.ts
@@ -11,10 +11,10 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales: ['en', 'es'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
 });
  
 export const config = {
