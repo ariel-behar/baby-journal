476cafc3b4161724730ea499a5462c1cd6e8fc06
Ariel Behar <ariel.behar@hotmail.com>
2023-11-09 09:09:01 +0200

    Implementing the LanguageSelector into the Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,16 +1,16 @@
-
-
+import LanguageSelector from "../LanguageSelector"
 import FooterMenu from "./FooterMenu"
 import FooterPoweredBy from "./FooterPoweredBy"
-
-
 
 function Footer() {
 	return (
 		<footer className="px-3 sm:px-5 lg:px-[50px] lg:container mx-auto flex flex-col justify-center sm:flex-row sm:justify-between h-[100px] items-center text-gray-500 text-sm border-t-[1px] border-gray-800">
 			<FooterPoweredBy />
 
-			<FooterMenu />
+			<div className="flex flex-row gap-x-5">
+				<FooterMenu />
+				<LanguageSelector />
+			</div>
 
 			<div className="text-center">
 				{new Date().getFullYear()} &copy; All Rights Reserved
