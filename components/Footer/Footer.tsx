dfbb3f18b4e95b52104511cbc9d401de41cc826d
Ariel Behar <ariel.behar@hotmail.com>
2023-11-11 20:00:27 +0200

    Aligning the Footer items to the center

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
 		<footer className="px-3 sm:px-5 lg:px-[50px] lg:container mx-auto flex flex-col justify-center sm:flex-row sm:justify-between h-[100px] items-center text-gray-500 text-sm border-t-[1px] border-gray-800">
 			<FooterPoweredBy />
 
-			<div className="flex flex-row gap-x-5">
+			<div className="flex flex-row items-center gap-x-5">
 				<FooterMenu />
 				<LanguageSelector />
 			</div>
