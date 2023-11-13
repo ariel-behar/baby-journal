6aaab61990eb626e3b5ed8dcc084856ccc039d33
Ariel Behar <ariel.behar@hotmail.com>
2023-11-13 11:38:17 +0200

    Changing text colors in the Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import LanguageSelector from "../LanguageSelector"
+import FooterAllRightsReserved from "./FooterAllRightsReserved"
 import FooterMenu from "./FooterMenu"
 import FooterPoweredBy from "./FooterPoweredBy"
 
 function Footer() {
 	return (
-		<footer className="px-3 sm:px-5 lg:px-[50px] lg:container mx-auto flex flex-col justify-center sm:flex-row sm:justify-between h-[100px] items-center text-gray-500 text-sm border-t-[1px] border-gray-800">
+		<footer className="px-3 sm:px-5 lg:px-[50px] lg:container mx-auto flex flex-col justify-center sm:flex-row sm:justify-between h-[100px] items-center text-sm border-t-[1px] border-gray-800">
 			<FooterPoweredBy />
 
 			<div className="flex flex-row items-center gap-x-5">
@@ -12,10 +13,7 @@ function Footer() {
 				<LanguageSelector />
 			</div>
 
-			<div className="text-center">
-				{new Date().getFullYear()} &copy; All Rights Reserved
-			</div>
-
+			<FooterAllRightsReserved />
 		</footer>
 	)
 }
--- components/Footer/FooterPoweredBy.tsx
+++ components/Footer/FooterPoweredBy.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 function FooterPoweredBy() {
     return (
-        <div className="mb-1 sm:mb-0 flex flex-row">
+        <div className="mb-1 sm:mb-0 flex flex-row text-muted">
             <span >
                 Powered by&nbsp;
                 <a href="https://www.arielbehar.com" className="font-bold mr-2 underline" target="_blank">
