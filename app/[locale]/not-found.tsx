684c8e84fe80e4267711107974ae4d274d5c1be7
Ariel Behar <ariel.behar@hotmail.com>
2023-11-13 09:06:00 +0200

    Adding an Image to the not-found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 function NotFound() {
@@ -6,6 +7,7 @@ function NotFound() {
             <h2>Not Found</h2>
             <p>Sorry, the page you are looking for does not exist</p>
             <Link href='/'>Return Home</Link>
+            <Image src="/img/baby-crying.png" alt="Not Found" width={300} height={300} />
         </div>
     )
 }
