23981ebabf614d2538ff1ecc44a080e2e13c2184
Ariel Behar <ariel.behar@hotmail.com>
2023-11-13 11:37:48 +0200

    Creating the FooterAllRightsReserved component

---
+++ components/Footer/FooterAllRightsReserved.tsx
@@ -0,0 +1,9 @@
+function FooterAllRightsReserved() {
+    return (
+        <div className="text-center text-muted">
+            {new Date().getFullYear()} &copy; All Rights Reserved
+        </div>
+    )
+}
+
+export default FooterAllRightsReserved
