b85b96e23742a5645077e74719a0067cebb5ef4f
Ariel Behar <ariel.behar@hotmail.com>
2023-11-15 09:14:14 +0200

    Optimizing the HomePage for mobile

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,35 +7,33 @@ export default function HomePage() {
 
 	return (
 		<div
-			className="flex-grow flex flex-col text-center items-start justify-center bg-no-repeat bg-contain bg-right"
+			className="flex-grow flex flex-col text-center items-center lg:items-start justify-center bg-no-repeat bg-contain bg-right-bottom"
 			style={{ backgroundImage: 'url("/img/mother-holding-baby.png"' }}
 		>
-			<section className="flex flex-col gap-[50px] items-center w-3/5" >
+			<section className="flex flex-col gap-[50px] items-center w-full lg:w-3/5" >
 				<div className="flex flex-col gap-y-5">
 
-					<h1 className="text-6xl md:text-8xl">{t('HomePage.title')}</h1>
-					<h3 className="text-xl md:text-2xl">{t('HomePage.subtitle')}</h3>
+					<h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl">{t('HomePage.title')}</h1>
+					<h3 className="text-lg lg:text-2xl">{t('HomePage.subtitle')}</h3>
 				</div>
 
-				<p className="text-xl">{t("HomePage.description")}</p>
-
-				<div className="w-full flex flex-row justify-between items-center px-40">
-					<div className="flex justify-center">
-						<img src='/img/crawling-baby-transparent.gif' alt="Hero" className="w-1/2" />
+				<div className="w-full flex flex-row justify-center items-center gap-x-3 lg:gap-x-10">
+					<div className="flex justify-center" >
+						<img src='/img/crawling-baby-transparent.gif' alt="Hero" className="max-h-[100px] lg:max-h-[150px] w-auto" />
 					</div>
 
-					<div className="flex gap-x-10">
-						<LinkButton href="/about" className="btn-lg btn-primary">
+					<div className="flex gap-x-3 lg:gap-x-10">
+						<LinkButton href="/about" className="lg:btn-lg btn-primary">
 							{t('Common.learn_more')}
 						</LinkButton>
 
-						<LinkButton href="/login" className="btn-lg btn-secondary">
+						<LinkButton href="/login" className="lg:btn-lg btn-secondary">
 							{t('Common.login')}
 						</LinkButton>
 					</div>
-
-
 				</div>
+
+				<p className="md:text-lg">{t("HomePage.description")}</p>
 			</section>
 
 
