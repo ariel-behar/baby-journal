5114a57b9de51b2b62b46770c51d6ad53784f153
Ariel Behar <ariel.behar@hotmail.com>
2023-11-16 22:57:36 +0200

    Adding the ContactForm component to the contact page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,3 +1,4 @@
+import ContactForm from "@/components/Forms/ContactForm";
 import { Metadata } from "next";
 import Image from "next/image"
 
@@ -8,8 +9,8 @@ export const metadata: Metadata = {
 
 function ContactPage() {
 	return (
-		<div className="flex flex-col md:flex-row gap-[100px] w-full justify-center items-center px-3 sm:px-5">
-			<div className="relative w-1/2 h-[90%] flex flex-row items-center">
+		<div className="flex flex-col md:flex-row lg:gap-[100px] w-full justify-center items-center px-3 sm:px-5">
+			<div className="relative w-1/2 h-[300px] lg:h-[90%] flex flex-row items-center">
 				<Image
 					src="/img/baby-open-arms.png"
 					alt="Contact"
@@ -19,14 +20,8 @@ function ContactPage() {
 			</div>
 
 			<div className="h-full flex items-center flex-1">
-				<form action="" className="flex flex-col gap-5 bg-dark-soft p-3 text-center rounded-md w-full">
-					<input type="text" placeholder="Name and Surname" className="form-input" />
-					<input type="email" placeholder="Email Address" className="form-input" />
-					<input type="text" placeholder="Phone Number (Optional)" className="form-input" />
-					<textarea name="" id="" cols={30} rows={10} placeholder="Message" className="form-input"></textarea>
-
-					<button className="btn btn-lg btn-primary">Send</button>
-				</form>
+				<ContactForm />
+				
 			</div>
 		</div>
 	)
