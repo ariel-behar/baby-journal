80774cf20a7fd878d26fbc53306091e0c03b8b3a
Ariel Behar <ariel.behar@hotmail.com>
2023-11-16 20:27:51 +0200

    Updating the button on the home page that leads to the blog with the proper url

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
 					</div>
 
 					<div className="flex gap-x-3 lg:gap-x-10">
-						<LinkButton href="/about" className="lg:btn-lg btn-primary">
+						<LinkButton href="/blog" className="lg:btn-lg btn-primary">
 							{t('Common.learn_more')}
 						</LinkButton>
 
