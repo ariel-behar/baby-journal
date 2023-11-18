cf623d7655eb4ee206924426eedbf37c529f389e
Ariel Behar <ariel.behar@hotmail.com>
2023-11-18 06:29:54 +0200

    Implementing the onFormSubmit functionality of the ContactForm

--- components/Forms/ContactForm.tsx
+++ components/Forms/ContactForm.tsx
@@ -1,9 +1,16 @@
 "use client"
 import { BaseSyntheticEvent } from 'react';
 import { useForm } from 'react-hook-form';
-import FormInputFieldWithTooltip from './FormComponents/FormInputFieldWithTooltip';
 import { yupResolver } from '@hookform/resolvers/yup';
+
+import { contactSchema } from '@/validation/contactSchema';
+
+import { useNotificationContext } from '@/context/notificationContext';
+
+import FormInputFieldWithTooltip from './FormComponents/FormInputFieldWithTooltip';
 import FormTextAreaWithTooltip from './FormComponents/FormTextAreaWithTooltip';
+import FormSubmitButton from './FormComponents/FormSubmitButton';
+import { sendContactMessage } from '@/lib/serverActions';
 
 export interface IContactFormData {
     firstName: string;
@@ -12,10 +19,10 @@ export interface IContactFormData {
     message: string;
 }
 
-
 function ContactForm() {
+    const { displayNotification } = useNotificationContext();
     const { register, handleSubmit, formState: { isDirty, isValid, errors } } = useForm<IContactFormData>({
-        // resolver: yupResolver(userLoginSchema),
+        resolver: yupResolver(contactSchema),
         mode: 'onBlur',
         defaultValues: {
             firstName: '',
@@ -25,30 +32,32 @@ function ContactForm() {
         },
     });
 
-    // const onFormSubmit = async (formData: ILoginFormData, e: BaseSyntheticEvent<object, any, any> | undefined) => {
-    //     e?.preventDefault();
+    const onFormSubmit = async (formData: IContactFormData, e: BaseSyntheticEvent<object, any, any> | undefined) => {
+        e?.preventDefault();
 
-    //     const { email, password } = formData;
+        const { firstName, lastName, email, message } = formData;
 
-    //     if (email && password) {
-    //         await loginUser(formData)
-    //             .then(res => {
-    //                 displayNotification('Successfully logged in!', 'info')
-    //             }).catch(error => {
-    //                 displayNotification(error.message, 'error')
-    //             })
-    //     }
-    // }
-    
+        if (firstName && lastName && email && message) {
+            await sendContactMessage(formData)
+                .then(res => {
+                    displayNotification('Successfully logged in!', 'info')
+                }).catch(error => {
+                    displayNotification(error.message, 'error')
+                })
+        }
+    }
+
     return (
-        <form action="" className="flex flex-col gap-5 bg-dark-soft p-3 text-center rounded-md w-full">
+        <form onSubmit={handleSubmit(onFormSubmit)} className="flex flex-col gap-5 bg-dark-soft p-3 text-center rounded-md w-full">
             <FormInputFieldWithTooltip register={register} errors={errors} name="firstName" label='First Name' type='text' />
             <FormInputFieldWithTooltip register={register} errors={errors} name="lastName" label='Last Name' type='text' />
             <FormInputFieldWithTooltip register={register} errors={errors} name="email" label='Email' type='email' />
 
             <FormTextAreaWithTooltip register={register} errors={errors} name="message" label='Message' />
 
-            <button className="btn btn-lg btn-primary">Send</button>
+            <FormSubmitButton className='btn-lg' isDirty={isDirty} isValid={isValid}>
+                Send
+            </FormSubmitButton>
         </form>
     )
 }
