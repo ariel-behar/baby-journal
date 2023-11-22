008e2577937d1e0aca909e0e1d6a01bb3906c6bb
Ariel Behar <ariel.behar@hotmail.com>
2023-11-22 17:05:27 +0200

    Adding semantic tags to the about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -9,21 +9,24 @@ export const metadata: Metadata = {
 function AboutPage() {
 	return (
 		<section className="flex flex-col xl:flex-row items-center gap-5 md:gap-10 xl:gap-[100px] px-3 sm:px-5">
-
-			<div className="w-full xl:w-1/2 flex flex-col gap-[50px] text-center md:text-left justify-center">
+			<article className="w-full xl:w-1/2 flex flex-col gap-[50px] text-center md:text-left justify-center">
 				<h3 className="text-3xl md:text-4xl lg:text-6xl font-bold leading-tight">About Baby Journal: Cherish Every Moment of Your Baby's Journey</h3>
 
-				<p className="text-xl text-justify">Baby Journal is the ultimate web app designed for mothers to document their baby's activities, milestones, and precious moments. Seamlessly record daily events, share tips, and track your baby's growth and development. With intuitive features, you can add photos, write descriptions and like other mothers' baby achievements. Baby Journal keeps your memories safe and organized, making it easy to reflect on your motherhood journey and share insights with other moms. Join our community and start creating a beautiful, personalized diary for your baby today!</p>
-			</div>
+				<div>
+					<p className="text-xl text-justify">Baby Journal is the ultimate web app designed for mothers to document their baby's activities, milestones, and precious moments.</p>
+					<br />
+					<p className="text-xl text-justify">Seamlessly record daily events, share tips, and track your baby's growth and development. With intuitive features, you can add photos, write descriptions and like other mothers' baby achievements. Baby Journal keeps your memories safe and organized, making it easy to reflect on your motherhood journey and share insights with other moms. Join our community and start creating a beautiful, personalized diary for your baby today!</p>
+				</div>
+			</article>
 
-			<div className="relative w-full xl:w-1/2 h-[200px] md:h-[300px] xl:h-[500px] ">
+			<figure className="relative w-full xl:w-1/2 h-[200px] md:h-[300px] xl:h-[500px] ">
 				<Image
 					src="/img/baby-upside-down.png"
 					alt="About Image"
 					fill
 					className="object-contain"
 				/>
-			</div>
+			</figure>
 		</section>
 	)
 }
