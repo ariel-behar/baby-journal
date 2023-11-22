7ac85e50550a23b89b3b5633cd787f969bba5b4a
Ariel Behar <ariel.behar@hotmail.com>
2023-11-22 16:38:15 +0200

    Adding semantic tags to the home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -11,11 +11,11 @@ async function HomePage() {
 	const user = (session as ICustomSession)?.user;
 
 	return (
-		<div
+		<section
 			className="flex-grow px-3 sm:px-5 flex flex-col text-center items-center lg:items-start justify-center bg-no-repeat bg-contain bg-right-bottom"
 			style={{ backgroundImage: 'url("/img/mother-holding-baby.png"' }}
 		>
-			<section className="flex flex-col gap-y-8 md:gap-[50px] items-center w-full lg:w-3/5" >
+			<article className="flex flex-col gap-y-8 md:gap-[50px] items-center w-full lg:w-3/5" >
 				<div className="flex flex-col gap-y-5">
 
 					<h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-8xl">{t('HomePage.title')}</h1>
@@ -23,9 +23,9 @@ async function HomePage() {
 				</div>
 
 				<div className="w-full flex flex-row justify-around sm:justify-center items-center gap-x-3 lg:gap-x-10">
-					<div className="flex justify-center" >
+					<figure className="flex justify-center" >
 						<img src='/img/crawling-baby-transparent.gif' alt="Hero" className="max-h-[70px] md:max-h-[100px] lg:max-h-[150px] w-auto" />
-					</div>
+					</figure>
 
 					<div className="flex gap-x-3 lg:gap-x-10">
 						<LinkButton href="/journal" className="lg:btn-lg btn-primary">
@@ -39,15 +39,11 @@ async function HomePage() {
 								</LinkButton>
 							)
 						}
-
 					</div>
 				</div>
-
 				<p className="md:text-lg">{t("HomePage.description")}</p>
-			</section>
-
-
-		</div>
+			</article>
+		</section>
 	);
 }
 
