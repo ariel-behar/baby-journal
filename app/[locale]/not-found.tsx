47285ec4da6e69dc8df3f9a415c0df02a2a6993a
Ariel Behar <ariel.behar@hotmail.com>
2023-11-23 20:41:48 +0200

    Adding semantic tags to the not-found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -3,12 +3,16 @@ import Image from "next/image"
 
 function NotFound() {
     return (
-        <div>
+        <section>
             <h2>Not Found</h2>
             <p>Sorry, the page you are looking for does not exist</p>
             <Link href='/'>Return Home</Link>
-            <Image src="/img/baby-crying.png" alt="Not Found" width={300} height={300} />
-        </div>
+            
+            <figure className="relative w-full h-[150px] sm:h-[300px] flex flex-row items-center">
+                <Image src="/img/baby-crying.png" alt="Not Found" fill className="object-contain" />
+            </figure>
+
+        </section>
     )
 }
 
