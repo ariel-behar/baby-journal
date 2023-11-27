9fd76ca287d874e0fb6dc670dfe6a1f5fed9102f
Ariel Behar <ariel.behar@hotmail.com>
2023-11-27 22:01:03 +0200

    Adding translations for the about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 import Image from "next/image"
 
 export const metadata: Metadata = {
@@ -6,16 +7,18 @@ export const metadata: Metadata = {
 	description: "Baby Journal is the ultimate web app designed for mothers to document their baby's activities, milestones, and precious moments. Seamlessly record daily events, share tips, and track your baby's growth and development. With intuitive features, you can add photos, write descriptions and like other mothers' baby achievements. Baby Journal keeps your memories safe and organized, making it easy to reflect on your motherhood journey and share insights with other moms. Join our community and start creating a beautiful, personalized diary for your baby today!"
 };
 
-function AboutPage() {
+async function AboutPage() {
+	const t = await getTranslations("AboutPage");
+
 	return (
 		<section className="flex flex-col xl:flex-row items-center gap-5 md:gap-10 xl:gap-[100px] px-3 sm:px-5">
 			<article className="w-full xl:w-1/2 flex flex-col gap-[50px] text-center md:text-left justify-center">
-				<h3 className="text-3xl md:text-4xl lg:text-6xl font-bold leading-tight">About Baby Journal: Cherish Every Moment of Your Baby's Journey</h3>
+				<h3 className="text-3xl md:text-4xl lg:text-6xl font-bold leading-tight">{t('title')}</h3>
 
 				<div>
-					<p className="text-xl text-justify">Baby Journal is the ultimate web app designed for mothers to document their baby's activities, milestones, and precious moments.</p>
+					<p className="text-xl text-justify">{t('paragraph-one')}</p>
 					<br />
-					<p className="text-xl text-justify">Seamlessly record daily events, share tips, and track your baby's growth and development. With intuitive features, you can add photos, write descriptions and like other mothers' baby achievements. Baby Journal keeps your memories safe and organized, making it easy to reflect on your motherhood journey and share insights with other moms. Join our community and start creating a beautiful, personalized diary for your baby today!</p>
+					<p className="text-xl text-justify">{t('paragraph-two')}</p>
 				</div>
 			</article>
 
