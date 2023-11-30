52dfead287a03b69c265efec4015c7b9f41be163
Ariel Behar <ariel.behar@hotmail.com>
2023-11-30 14:02:51 +0200

    Implementing keys for translation in the contactFormSchema

--- validation/contactFormSchema.ts
+++ validation/contactFormSchema.ts
@@ -8,23 +8,23 @@ export interface IContactFormYupSchema extends IContactFormData {}
 const contactFormSchemaShape = {
     firstName: yup
         .string()
-        .required('First name is required')
-        .min(2, 'First name must be at least 2 characters long')
-        .max(20, 'First name must be at most 20 characters long'),
+        .required('first-name-is-required')
+        .min(2, 'first-name-must-be-at-least-2-characters-long')
+        .max(20, 'first-name-must-be-at-most-20-characters-long'),
     lastName: yup
         .string()
-        .required('Last name is required')
-        .min(2, 'Last name must be at least 2 characters long')
-        .max(20, 'Last name must be at most 20 characters long'),
+        .required('last-name-is-required')
+        .min(2, 'last-name-must-be-at-least-2-characters-long')
+        .max(20, 'last-name-must-be-at-most-20-characters-long'),
     email: yup
         .string()
-        .matches(emailRegex, 'Email is not valid')
-        .required("Email is required"),
+        .matches(emailRegex, 'email-is-not-valid')
+        .required('email-is-required'),
     message: yup
         .string()
-        .required('Message is required')
-        .min(50, 'Message must be at least 50 characters long')
-        .max(500, 'Message must be at most 500 characters long'),
+        .required('message-is-required')
+        .min(50, 'message-must-be-at-least-50-characters-long')
+        .max(500, 'message-must-be-at-most-500-characters-long'),
 }
 
 const contactFormSchema: yup.ObjectSchema<IContactFormYupSchema> = yup.object().shape(contactFormSchemaShape);
