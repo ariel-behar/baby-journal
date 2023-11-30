ea49b45b07bf0e18723ac2a360f2067d84ec5479
Ariel Behar <ariel.behar@hotmail.com>
2023-11-30 14:06:20 +0200

    Implementing keys for translation in the postSchema

--- validation/postSchema.ts
+++ validation/postSchema.ts
@@ -6,20 +6,20 @@ interface IPostYupSchema extends Omit<IPost, "_id" | 'createdAt' | "updatedAt" |
 const postSchemaShape = {
     title: yup
         .string()
-        .required('Title is required')
-        .min(3, 'Title must be at least 3 characters long')
-        .max(50, 'Title must be at most 50 characters long'),
+        .required('title-is-required')
+        .min(3, 'title-must-be-at-least-3-characters-long')
+        .max(50, 'title-must-be-at-most-50-characters-long'),
     description: yup
         .string()
-        .required('Description is required')
-        .min(50, 'Description must be at least 50 characters long')
-        .max(1000, 'Description must be at most 1000 characters long'),
+        .required('description-is-required')
+        .min(50, 'description-must-be-at-least-50-characters-long')
+        .max(1000, 'description-must-be-at-most-1000-characters-long'),
     user: yup
         .string()
-        .required('User ID is required'),
+        .required('user-id-is-required'),
     img: yup
         .string()
-        .required('Image is required')
+        .required('image-is-required')
 }
 
 export const postSchema: yup.ObjectSchema<IPostYupSchema> = yup.object().shape(postSchemaShape);
