d71be9ae6cd9f503082d2a9b72808e68c20769d6
Ariel Behar <ariel.behar@hotmail.com>
2023-11-30 14:01:45 +0200

    Addapting the FormErrorTooltip for translation of validation messages

--- components/Forms/FormComponents/FormErrorTooltip.tsx
+++ components/Forms/FormComponents/FormErrorTooltip.tsx
@@ -4,6 +4,7 @@ import { ILoginFormData } from "../LoginForm";
 import { IRegisterFormData } from "../RegisterForm";
 import { IPostFormData } from "../AddEditPostForm";
 import { IContactFormData } from "../ContactForm";
+import { useTranslations } from "next-intl";
 
 interface Props {
     name: keyof ILoginFormData | keyof IRegisterFormData | keyof IPostFormData | keyof IContactFormData;
@@ -15,10 +16,13 @@ function FormErrorTooltip({
     name,
     errors,
     children
-
 }:Props) {
+    const t = useTranslations("Forms.validation");
+
     return (
-        <div className={`tooltip tooltip-top ${errors[name] && 'tooltip-open'} tooltip-error`} data-tip={errors[name] && errors[name]?.message}>
+        <div 
+        className={`tooltip tooltip-top ${errors[name] && 'tooltip-open'} tooltip-error`} 
+        data-tip={errors[name] && t(errors[name]?.message as any)}>
             {children}
         </div>
     )
