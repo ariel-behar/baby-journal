9029f2edaa8c33adda2510f85829529dfc416a00
Ariel Behar <ariel.behar@hotmail.com>
2023-11-30 11:38:22 +0200

    Adding translations to the Login/Register pages

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -1,20 +1,22 @@
+import { getTranslations } from "next-intl/server";
 import { Link } from "@/lib/i18nNavigation";
 
 import FormStylesWrapper from "@/components/Forms/FormComponents/FormStylesWrapper";
 import LoginForm from "@/components/Forms/LoginForm";
 
 async function LoginPage() {
+	const t = await getTranslations('LoginPage');
 
 	return (
 		<section className="flex-grow flex items-center justify-center">
-			<FormStylesWrapper title="Login" className="p-3 md:p-12 flex flex-col w-full md:w-[500px]">
+			<FormStylesWrapper title={t('login')} className="p-3 md:p-12 flex flex-col w-full md:w-[500px]">
 				
 				<LoginForm />
 
 				<span className="my-3">
-					Don't have an account?&nbsp;
+					{t('dont-have-an-account')}&nbsp;
 					<Link href="/register" className="font-bold">
-						Register here!
+						{t('register-here')}
 					</Link>
 				</span>
 			</FormStylesWrapper>
--- app/[locale]/(auth)/register/page.tsx
+++ app/[locale]/(auth)/register/page.tsx
@@ -1,17 +1,20 @@
+import { getTranslations } from "next-intl/server"
 import { Link } from "@/lib/i18nNavigation"
 
 import RegisterForm from "@/components/Forms/RegisterForm"
 import FormStylesWrapper from "@/components/Forms/FormComponents/FormStylesWrapper"
 
-function RegisterPage() {
+async function RegisterPage() {
+	const t = await getTranslations('RegisterPage')
+
 	return (
 		<section className="flex-grow flex items-center justify-center">
-			<FormStylesWrapper title="Register" className="p-3 md:p-12 flex flex-col w-full md:w-[500px]">
+			<FormStylesWrapper title={t('register')} className="p-3 md:p-12 flex flex-col w-full md:w-[500px]">
 				<RegisterForm />
 				<span className="my-3">
-					Already an existing user?&nbsp;
+					{t('already-an-existing-user')}&nbsp;
 					<Link href="/login" className="font-bold">
-						Login here!
+						{t('login-here')}
 					</Link>
 				</span>
 			</FormStylesWrapper>
