f743dbb7b6785a1f25800f1588075571b20c5793
Ariel Behar <ariel.behar@hotmail.com>
2023-12-02 14:45:33 +0200

    Adding a translation for 'Logo' in the Navbar

--- components/Header/Navbar/Navbar.tsx
+++ components/Header/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
-import UserNavigationMenu from "./UserNavigationMenu"
+import { getTranslations } from "next-intl/server";
+import { Session } from "next-auth";
+
 import { auth } from "@/lib/auth";
+import { Link } from "@/lib/i18nNavigation";
+
+import { routesFooter, routesMain } from "@/data/routes"
 
 import { ICustomSession } from "@/types/types";
-import { Session } from "next-auth";
 
 import DesktopMainNavigationMenu from "./DesktopMainNavigationMenu";
 import MobileMainNavigationMenu from "./MobileMainNavigationMenu";
-
-import { routesFooter, routesMain } from "@/data/routes"
-import { Link } from "@/lib/i18nNavigation";
+import UserNavigationMenu from "./UserNavigationMenu"
 
 async function Navbar() {
+	const t = await getTranslations()
 	const session: Session | null = await auth();
 	const user: ICustomSession["user"] = (session as ICustomSession)?.user
 
@@ -19,7 +22,7 @@ async function Navbar() {
 
 			<div className="h-[80px] md:h-full w-[90px] md:w-[110px] md:hover:w-[230px] transition-all duration-200 relative overflow-hidden py-1">
 				<Link href="/" className="text-3xl font-bold h-full w-full">
-					<img src="/img/baby-crawling-home.png" alt="Logo" className="w-full h-full object-cover" style={{ objectPosition: 0 }} />
+					<img src="/img/baby-crawling-home.png" alt={t('Common.logo')} className="w-full h-full object-cover" style={{ objectPosition: 0 }} />
 				</Link>
 			</div>
 
