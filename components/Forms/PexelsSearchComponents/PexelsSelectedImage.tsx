2606be51acd32e9cafaf55013cafaf0446e2d040
Ariel Behar <ariel.behar@hotmail.com>
2023-12-03 19:58:11 +0200

    Creating the PexelsSelectedImage component

---
+++ components/Forms/PexelsSearchComponents/PexelsSelectedImage.tsx
@@ -0,0 +1,22 @@
+import { Photo } from 'pexels';
+import Image from 'next/image';
+
+import { IPost } from '@/models/Post';
+
+interface Props {
+    selectedPhotoObject: Photo | null,
+    selectedPhoto: IPost['img'] | string | undefined
+}
+
+function PexelsSelectedImage({
+    selectedPhotoObject,
+    selectedPhoto,
+}: Props) {
+    return (
+        <figure className='h-[250px] w-[250px] relative overflow-hidden'>
+            <Image className='object-cover rounded-md' src={selectedPhotoObject?.src.medium || selectedPhoto || ''} alt={selectedPhotoObject?.alt ? selectedPhotoObject.alt : "Pexels Photo"} fill />
+        </figure>
+    )
+}
+
+export default PexelsSelectedImage
