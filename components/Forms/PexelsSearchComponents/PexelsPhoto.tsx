31d236980a46c78e4b170a4ced5d6b9b43555ca0
Ariel Behar <ariel.behar@hotmail.com>
2023-12-03 19:56:40 +0200

    Creating the PexelsPhoto component

---
+++ components/Forms/PexelsSearchComponents/PexelsPhoto.tsx
@@ -0,0 +1,25 @@
+import { Photo } from 'pexels';
+import Image from 'next/image';
+
+interface Props {
+    photo: Photo
+    handleSelectPhoto: (photo: Photo) => void
+}
+
+function PexelsPhoto({
+    photo,
+    handleSelectPhoto,
+}: Props) {
+    return (
+        <button
+            className='hover:scale-110 transition-all duration-300 hover:z-40 hover:rounded-md border-2 hover:border-white border-transparent box-border'
+            onClick={() => handleSelectPhoto(photo)}
+        >
+            <figure className='h-[140px] w-[140px] relative overflow-hidden'>
+                <Image className='object-cover ' src={photo.src.tiny} alt={photo.alt ? photo.alt : "Pexels Photo"} fill />
+            </figure>
+        </button>
+    )
+}
+
+export default PexelsPhoto
