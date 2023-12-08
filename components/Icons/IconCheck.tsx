e9c91c071e3d572fddf1e3b31d6bca4f92363034
Ariel Behar <ariel.behar@hotmail.com>
2023-12-08 09:55:46 +0200

    Adding a 'stroke' and 'strokeWidth' to the IconCheck component

--- components/Icons/IconCheck.tsx
+++ components/Icons/IconCheck.tsx
@@ -1,10 +1,17 @@
 import { IIconProps } from "@/types/types"
 
+interface Props extends IIconProps {
+    stroke?: string,
+    strokeWidth?: number
+}
+
 function IconCheck({
-    sizeClassName = 'size-5'
-}:IIconProps) {
+    sizeClassName = 'size-5',
+    stroke = 'currentColor',
+    strokeWidth = 1.5
+}:Props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sizeClassName}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke={stroke} className={sizeClassName}>
             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
         </svg>
     )
