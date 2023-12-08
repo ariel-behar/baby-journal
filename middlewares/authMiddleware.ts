561a129add6aecd535d7b75018adf944ff5a1d40
Ariel Behar <ariel.behar@hotmail.com>
2023-12-08 21:00:40 +0200

    Modifying the authMiddleware to use authConfig authorized callbacks for route-guarding

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -1,24 +1,11 @@
-import { NextResponse } from "next/server"
 import NextAuth from "next-auth"
 
-import { authPages } from "@/middleware"
 import { intlMiddleware } from "./i18nMiddleware"
-import { testPathNameRegex } from "@/utils/testPathNameRegex"
 import { authConfig } from "@/lib/auth.config"
 
-const authMiddleware = NextAuth(authConfig).auth((req: any) => {
-	const isAuthPage = testPathNameRegex(authPages, req.nextUrl.pathname)
-	const session = req.auth
-
-	// Redirect to sign-in page if not authenticated
-	if (!session && !isAuthPage) {
-		return NextResponse.redirect(new URL("/login", req.nextUrl))
-	}
-
-	// Redirect to home page if authenticated and trying to access auth pages
-	if (session && isAuthPage) {
-		return NextResponse.redirect(new URL("/", req.nextUrl))
-	}
+const authMiddleware =  NextAuth(authConfig).auth((req: any) => {
+	
+	authConfig.callbacks.authorized({ auth: req.auth, request: req })
 
 	return intlMiddleware(req)
 })
