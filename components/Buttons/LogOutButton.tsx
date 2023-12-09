18c245b0b3b43894b309c97676578c6fee172963
Ariel Behar <ariel.behar@hotmail.com>
2023-12-09 11:11:04 +0200

    Adding optional buttonClasses/formClasses to the LogOutButton

--- components/Buttons/LogOutButton.tsx
+++ components/Buttons/LogOutButton.tsx
@@ -3,12 +3,20 @@ import { handleLogout } from '@/lib/serverActions'
 
 import IconLogOut from '../Icons/IconLogOut'
 
-function LogOutButton() {
+interface Props {
+    formClasses?: string,
+    buttonClasses?: string
+}
+
+function LogOutButton({
+    formClasses = 'mx-auto',
+    buttonClasses
+}: Props) {
     const t = useTranslations('Common')
 
     return (
-        <form className="mx-auto" action={handleLogout}>
-            <button className="btn btn-sm rounded-2xl px-2 py-1 flex justify-between text-light hover:bg-light hover:text-dark">
+        <form className={`${formClasses}`} action={handleLogout}>
+            <button className={`${buttonClasses} btn btn-sm rounded-2xl px-2 py-1 flex justify-between text-light hover:bg-light hover:text-dark`}>
                 {t('logout')} <IconLogOut />
             </button>
         </form>
