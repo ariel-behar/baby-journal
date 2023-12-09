8853122b012484bf275c083ed0d0d5c00c5b5ea1
Ariel Behar <ariel.behar@hotmail.com>
2023-12-09 10:11:14 +0200

    Optimizing the grid PexelPhoto sizes on small screens

--- components/Forms/PexelsSearchComponents/PexelsPhoto.tsx
+++ components/Forms/PexelsSearchComponents/PexelsPhoto.tsx
@@ -15,7 +15,7 @@ function PexelsPhoto({
             className='hover:scale-110 transition-all duration-300 hover:z-40 hover:rounded-md border-2 hover:border-white border-transparent box-border'
             onClick={() => handleSelectPhoto(photo)}
         >
-            <figure className='h-[140px] w-[140px] relative overflow-hidden'>
+            <figure className='h-[90px] w-[90px] sm:h-[140px] sm:w-[140px] relative overflow-hidden'>
                 <Image className='object-cover ' src={photo.src.tiny} alt={photo.alt ? photo.alt : "Pexels Photo"} fill />
             </figure>
         </button>
