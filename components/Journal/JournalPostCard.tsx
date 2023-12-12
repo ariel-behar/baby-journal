7a35ad8628b3b1274a331d0f46e49fa7ac4eb463
Ariel Behar <ariel.behar@hotmail.com>
2023-12-12 11:21:18 +0200

    Optimizing the JournalPostCard for extra small screens

--- components/Journal/JournalPostCard.tsx
+++ components/Journal/JournalPostCard.tsx
@@ -9,6 +9,7 @@ import { IPostPopulated } from "@/models/Post";
 import IconChevronRight from "../Icons/IconChevronRight";
 import LikeButton from "../Buttons/LikeButton";
 import LikeCounter from "./LikeCounter";
+import LinkButton from "../Buttons/LinkButton";
 
 interface Props {
 	post: IPostPopulated,
@@ -25,39 +26,48 @@ async function JournalPostCard({
 	return (
 		<article className="card w-full bg-dark-soft text-primary-content shadow-xl">
 			<Link href={`/journal/${post._id}`}>
-				<figure className="w-full h-[300px] relative rounded-t-xl">
+				<figure className="w-full h-[100px] sm:h-[300px] relative rounded-t-xl">
 					<Image className="object-cover transform hover:scale-105 duration-700" src={post.img} alt={t('post')} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" priority />
 				</figure>
 			</Link>
 
-			<div className="card-body p-4 md:p-8 gap-y-0">
+			<div className="card-body p-2 sm:p-4 md:p-8 gap-y-0">
+
+				{/* On Extra small screen */}
+				<time className="block sm:hidden text-xs text-muted text-end mb-0">
+					{format(new Date(post.createdAt), "dd-MM-yyyy")}
+				</time>
+
 				<div className="flex flex-row justify-between items-start mb-1 w-full">
-					<span className="text-sm text-muted">
+					<span className="text-xs sm:text-sm text-muted">
 						{t('author').toLowerCase()} {post.user.firstName} {post.user.lastName}
 					</span>
-					<time className="text-sm text-muted text-end mb-0">
+
+
+					{/* From small screens onwards */}
+					<time className="hidden sm:block text-sm text-muted text-end mb-0">
 						{format(new Date(post.createdAt), "dd-MM-yyyy")}
 					</time>
 				</div>
 
-				<h4 className="card-title w-full break-words inline-block">
+				<h4 className="card-title w-full break-words inline-block text-base sm:text-xl">
 					<Link href={`/journal/${post._id}`}>
 						{post.title}
 					</Link>
 				</h4>
 
-				<div className="card-actions justify-between items-center mt-2 flex-grow">
+				<div className="card-actions justify-between items-center mt-0 sm:mt-2 flex-grow">
 					<div className="flex flex-row items-center gap-2 min-h-[20px]">
-						{ !(user?.id == post.user._id) && <LikeButton post={post} user={user} /> }
+						{!(user?.id == post.user._id) && <LikeButton post={post} user={user} />}
 
 						<LikeCounter isPostOwner={user?.id == post.user._id} likes={post.likes} />
 					</div>
 
 					<div className="flex flex-row justify-end items-end w-full">
-						<Link href={`/journal/${post._id}`} className="btn btn-primary btn-sm btn-min-width uppercase">
+						<LinkButton href={`/journal/${post._id}`} >
 							{t('read-more')}
 							<IconChevronRight />
-						</Link>
+						</LinkButton>
 					</div>
 				</div>
 			</div>
