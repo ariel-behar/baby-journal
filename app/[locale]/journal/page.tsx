5a1fe6cb63eaa195888c3525c12c143d214ded95
Ariel Behar <ariel.behar@hotmail.com>
2023-12-12 11:20:49 +0200

    Making the journal page to be a 2 columns grid

--- app/[locale]/journal/page.tsx
+++ app/[locale]/journal/page.tsx
@@ -35,7 +35,7 @@ async function JournalPage() {
 
 			{
 				posts.length > 0
-					? <div className="py-5 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 gap-y-5 lg:gap-5">
+					? <div className="py-5 grid grid-cols-2 sm:grid-cols-2 xl:grid-cols-3 gap-2 sm:gap-3 gap-y-5 lg:gap-5">
 
 						{
 							posts.map((post) => (
